1986d815981995dc1a913412938a60a3fa6617d7
Marco Eilers <marcoeilers@users.noreply.github.com>
2016-02-08 16:10:40 +0100

    Another small bug fix, added more exception tests since they actually work now

--- tests/verification/test_exception.py
+++ tests/verification/test_exception.py
@@ -127,3 +127,70 @@ def raisedAndCaught2(out: Container) -> None:
         out.value = 2 * tmp.value
     except MyException:
         out.value = 3 * tmp.value
+
+def raisedAndCaught3(out: Container) -> None:
+    Requires(Acc(out.value))
+    #:: ExpectedOutput(postcondition.violated:assertion.false)
+    Ensures(Acc(out.value) and out.value == 13)
+    Exsures(MyException, False)
+    try:
+        raise MyException()
+    except MyException:
+        out.value = 12
+
+
+def raisedAndCaught4(out: Container) -> None:
+    Requires(Acc(out.value))
+    #:: ExpectedOutput(postcondition.violated:assertion.false)
+    Ensures(Acc(out.value) and out.value == 39)
+    tmp = Container()
+    try:
+        helper(tmp, 45)
+        out.value = 2 * tmp.value
+    except MyException:
+        out.value = 3 * tmp.value
+
+def nested(out: Container) -> None:
+    Requires(Acc(out.value))
+    Ensures(Acc(out.value) and out.value == 1 )
+    try:
+
+        try:
+            raise MyException()
+            Assert(False)
+        except MyException:
+            out.value = 33
+        except Exception:
+            Assert(False)
+        if out.value == 33:
+            raise MyOtherException()
+            Assert(False)
+        else:
+            Assert(False)
+        Assert(False)
+    except MyOtherException:
+        out.value = -1
+    out.value *= out.value
+
+def nested2(out: Container) -> None:
+    Requires(Acc(out.value))
+    Ensures(Acc(out.value) and out.value == 1 )
+    try:
+
+        try:
+            raise MyException()
+            out.value = 33
+        except MyException:
+            out.value = 34
+        except Exception:
+            Assert(False)
+        if out.value == 33:
+            raise MyOtherException()
+            Assert(False)
+        else:
+            #:: ExpectedOutput(assert.failed:assertion.false)
+            Assert(False)
+        Assert(False)
+    except MyOtherException:
+        out.value = -1
+    out.value *= out.value
