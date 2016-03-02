6d816d7db73ba8a9ceb6619d0d391d5957994043
Vytautas Astrauskas <vakaras@users.noreply.github.com>
2016-03-02 16:43:26 +0100

    Rewrote py2viper script.

--- src/py2viper_translation/main.py
+++ src/py2viper_translation/main.py
@@ -1,8 +1,10 @@
 import ast
-from py2viper_translation import astpp
 import os
 import sys
+import argparse
 
+from py2viper_translation import astpp
+from py2viper_translation import config
 from py2viper_translation.analyzer import Analyzer
 from jpype import JavaException
 from py2viper_translation.jvmaccess import JVM
@@ -28,7 +30,6 @@ def translate(path: str, jvm: JVM):
             with open(path, 'r') as file:
                 text = file.read()
             parseresult = ast.parse(text)
-            print(astpp.dump(parseresult))
             viperast = ViperAST(jvm, jvm.java, jvm.scala, jvm.viper, path)
             translator = Translator(jvm, path, types, viperast)
             analyzer = Analyzer(jvm, viperast, types)
@@ -58,27 +59,59 @@ def verify(prog: 'viper.silver.ast.Program', path: str,
         print(je.stacktrace())
 
 
-def main_translate() -> None:
-    if len(sys.argv) < 3:
-        print("Usage: py2viper py_file_path viper_jar_path [mypy_path]")
-        exit(1)
-    path = sys.argv[1]
-    viperjar = sys.argv[2]
+def main() -> None:
+    """ Entry point for the translator.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'python_file',
+        help='Python file to verify')
+    parser.add_argument(
+        '--viper-jar-path',
+        help='Java CLASSPATH that includes Viper class files',
+        default=config.classpath)
+    parser.add_argument(
+        '--boogie',
+        help='path to Boogie executable',
+        default=config.boogie_path)
+    parser.add_argument(
+        '--z3',
+        help='path to Z3 executable',
+        default=config.z3_path)
+    parser.add_argument(
+        '--mypy-path',
+        help='mypy path',
+        default=config.mypy_path)
+    parser.add_argument(
+        '--print-silver',
+        action='store_true',
+        help='print generated Silver program')
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="increase output verbosity")
+    args = parser.parse_args()
+
+    python_file = args.python_file
+    config.classpath = args.viper_jar_path
+    config.boogie_path = args.boogie
+    config.z3_path = args.z3
+    config.mypy_path = args.mypy_path
+
+    os.environ['MYPYPATH'] = config.mypy_path
+    jvm = JVM(config.classpath)
     try:
-        mypydir = sys.argv[3]
-    except IndexError:
-        mypydir = get_mypy_dir()
-        if mypydir is None:
-            print(
-                "Could not find mypy. Please provide path to mypy as third argument.")
-            exit()
-    jvm = JVM(viperjar)
-    try:
-        prog = translate(path, jvm, mypydir)
-        print("Translation successful. Result:")
-        print(prog)
-        vresult = verify(prog, path, jvm)
-        print("Verification completed.")
+        prog = translate(python_file, jvm)
+        if args.verbose:
+            print('Translation successful.')
+        if args.print_silver:
+            if args.verbose:
+                print('Result:')
+            print(prog)
+        vresult = verify(prog, python_file, jvm)
+        if args.verbose:
+            print("Verification completed.")
         print(vresult)
     except (TypeException, InvalidProgramException) as e:
         print("Translation failed")
@@ -89,4 +122,4 @@ def main_translate() -> None:
 
 
 if __name__ == '__main__':
-    main_translate()
+    main()
