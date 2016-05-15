e715870771aef16d556bb8978a1e80588ced1f16
Vytautas Astrauskas <vakaras@users.noreply.github.com>
2016-05-15 18:21:41 +0200

    Added regression tests for issues 1, 2, and 3.

---
+++ tests/verification/issues/00003.py
@@ -0,0 +1,9 @@
+#:: IgnoreFile(/py2viper/issue/2/)
+from py2viper_contracts.contracts import *
+
+def break_out() -> bool:
+    while True:
+        try:
+            raise Exception()
+        finally:
+            break
