e3d985c0d45183c28ddfc94d01c2271ec5915b5f
Marco Eilers <marco.eilers@inf.ethz.ch>
2016-05-24 15:50:57 +0200

    Fixed parameter names in translation tests

--- tests/translation/test_behavioural_subtyping_2.py
+++ tests/translation/test_behavioural_subtyping_2.py
@@ -11,12 +11,12 @@ class SuperA:
 
 
 class SubA(SuperA):
-    def some_method(self, b: int) -> int:
-        return b + 5
+    def some_method(self, a: int) -> int:
+        return a + 5
 
 class SubSubA(SubA):
-    def some_method(self, b: int) -> int:
-        return b + 9
+    def some_method(self, a: int) -> int:
+        return a + 9
 
 class SuperF:
     def some_method(self, b: SubA, a: SubSubA) -> SubA:
@@ -24,5 +24,5 @@ class SuperF:
 
 class SubF1(SuperF):
     #:: ExpectedOutput(type.error:Argument 1 of "some_method" incompatible with supertype "SuperF")
-    def some_method(self, a : SubSubA, b : SubSubA) -> SubA:
-        return b
+    def some_method(self, b: SubSubA, a: SubSubA) -> SubA:
+        return a
