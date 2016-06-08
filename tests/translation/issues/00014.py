1b1db7eed37bd61d7b1ee19d9e2872450fbd890e
Marco Eilers <marcoeilers@users.noreply.github.com>
2016-06-08 15:34:17 +0000

    Created new branch bugfix

---
+++ tests/translation/issues/00014.py
@@ -0,0 +1,4 @@
+#:: IgnoreFile(/py2viper/issue/14/)
+from py2viper_contracts.contracts import *
+
+Requires(True)
---
+++ tests/verification/issues/00008.py
@@ -0,0 +1,5 @@
+#:: IgnoreFile(/py2viper/issue/8/)
+from py2viper_contracts.contracts import *
+
+def test1() -> None:
+    return
