2e04e379b07007bec9aa4ce6053bf9bf9d34cc46
Samuel Hitz <shitz@users.noreply.github.com>
2016-06-08 19:17:25 +0200

    addressed comments from review

--- src/py2viper_translation/sif/lib/context.py
+++ src/py2viper_translation/sif/lib/context.py
@@ -48,10 +48,10 @@ class SIFContext(Context):
             return self._curr_tl_var_expr
         elif TL_VAR_NAME in self.var_aliases:
             return self.var_aliases[TL_VAR_NAME].ref
-        elif self.current_function.pure:
-            return self.current_function.tl_var.ref
+        elif self.actual_function.pure:
+            return self.actual_function.tl_var.ref
         else:
-            return self.current_function.new_tl_var.ref
+            return self.actual_function.new_tl_var.ref
 
     @current_tl_var_expr.setter
     def current_tl_var_expr(self, expr: Expr):
