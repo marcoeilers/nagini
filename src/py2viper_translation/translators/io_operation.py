885d968a934e95375a0f5db2e59821164e7b4674
Vytautas Astrauskas <vakaras@users.noreply.github.com>
2016-06-20 17:25:47 +0200

    Made linters happy.

--- src/py2viper_translation/translators/io_operation.py
+++ src/py2viper_translation/translators/io_operation.py
@@ -1,32 +1,47 @@
+"""
+This file contains code responsible for translating IO operations.
+"""
+
+
 from py2viper_translation.lib.program_nodes import (
     PythonIOOperation,
     PythonVar,
-    )
+)
 from py2viper_translation.translators.abstract import Context
 from py2viper_translation.translators.common import CommonTranslator
 from typing import Tuple, List
 
-if False:         # Just to make mypy happy.
-    import viper  # noqa
+# Just to make mypy happy.
+if False:         # pylint: disable=using-constant-test
+    import viper  # pylint: disable=import-error,unused-import
+
+
+def _construct_getter_name(operation: PythonIOOperation,
+                           result: PythonVar) -> str:
+    """ Utility function for constructing getter name.
+    """
+    return 'get__{0}__{1}'.format(
+        operation.sil_name,
+        result.sil_name,
+    )
 
 
 class IOOperationTranslator(CommonTranslator):
+    """ Class responsible for translating IO operations.
+    """
 
     def translate_io_operation(
-            self,
-            operation: PythonIOOperation,
-            ctx: Context,
-            ) -> Tuple[
+            self, operation: PythonIOOperation,
+            ctx: Context) -> Tuple[
                 'viper.silver.ast.Predicate',
                 List['viper.silver.ast.Function'],
-                List['viper.silver.ast.Method'],
-                ]:
+                List['viper.silver.ast.Method']]:
         """ Translates IO operation to Silver.
         """
         args = [
             arg.decl
             for arg in operation.get_arguments()
-            ]
+        ]
         position = self.to_position(operation.node, ctx)
         info = self.no_info(ctx)
 
@@ -35,7 +50,7 @@ class IOOperationTranslator(CommonTranslator):
 
         getters = []
         for result in operation.get_results():
-            name = self._construct_getter_name(operation, result)
+            name = _construct_getter_name(operation, result)
             typ = self.translate_type(result.type, ctx)
             getter = self.viper.Function(name, args, typ, [], [], None,
                                          position, info)
@@ -45,11 +60,4 @@ class IOOperationTranslator(CommonTranslator):
             predicate,
             getters,
             []
-            )
-
-    def _construct_getter_name(self, operation: PythonIOOperation,
-                               result: PythonVar) -> str:
-        return 'get__{0}__{1}'.format(
-            operation.sil_name,
-            result.sil_name,
-            )
+        )
