29be7d3bdcaa71ad35d4fe2daa8d84f68a3113f0
Vytautas Astrauskas <vakaras@users.noreply.github.com>
2016-06-24 14:58:43 +0200

    Added Hello World test.

---
+++ tests/verification/io/library.py
@@ -0,0 +1,53 @@
+from py2viper_contracts.contracts import (
+    Requires,
+    Predicate,
+    Result,
+    Assert,
+)
+from py2viper_contracts.io import *
+from typing import Tuple, Callable
+
+
+@IOOperation
+def write_string_io(
+        t_pre: Place,
+        value: str,
+        t_post: Place = Result(),
+        ) -> bool:
+    Terminates(True)
+
+
+def write_string(t1: Place, value: str) -> Place:
+    IOExists = lambda t2: (
+        Requires(
+            token(t1) and
+            write_string_io(t1, value, t2)
+        ),
+        Ensures(
+            token(t2) and
+            t2 == Result()
+        )
+    )   # type: Callable[[Place], Tuple[bool, bool]]
+
+
+@IOOperation
+def read_int_io(
+        t_pre: Place,
+        number: int = Result(),
+        t_post: Place = Result(),
+        ) -> bool:
+    Terminates(False)
+
+
+def read_int(t1: Place) -> Tuple[Place, int]:
+    IOExists = lambda t2, value: (
+        Requires(
+            token(t1) and
+            read_int_io(t1, value, t2)
+        ),
+        Ensures(
+            token(t2) and
+            (t2 == Result()[0] and
+            value == Result()[1])
+        )
+    )   # type: Callable[[Place, int], Tuple[bool, bool]]
