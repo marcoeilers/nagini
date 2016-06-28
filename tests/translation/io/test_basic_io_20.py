d1fe9c703cb28a4fe602e479e34b09a23062ccae
Vytautas Astrauskas <vakaras@users.noreply.github.com>
2016-06-28 15:12:50 +0200

    Updated forgotten test with new IOExists syntax.

--- tests/translation/io/test_basic_io_20.py
+++ tests/translation/io/test_basic_io_20.py
@@ -1,15 +1,17 @@
 from py2viper_contracts.contracts import Requires, Predicate
 from py2viper_contracts.io import *
-from typing import Tuple, Callable
+from typing import Tuple
 
 
 def test() -> Place:
-    IOExists = lambda t2: (
+    IOExists1(Place)(
+        lambda t2: (
         Ensures(
             t2 == Result() and
             token(t2)
+        ),
         )
-    )   # type: Callable[[Place], bool]
+    )
 
     #:: ExpectedOutput(type.error:Name 't2' is not defined)
     a = t2
