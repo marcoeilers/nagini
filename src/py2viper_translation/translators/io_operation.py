97d743122dedb2ca4ed67229cec468f159a96d0a
Vytautas Astrauskas <vastrauskas@gmail.com>
2016-07-03 16:25:20 +0200

    Added tests for IO behavioral subtyping.

--- src/py2viper_translation/translators/io_operation.py
+++ src/py2viper_translation/translators/io_operation.py
@@ -62,6 +62,79 @@ contract, which must be one of:
             (b ? True : x.f == x.g) && x.f == 2
 
         which is probably not what a programmer intended.
+
+------------
+Known Issues
+------------
+
+Heap Dependent Getters
+----------------------
+
+If one of the IO operation arguments is a field, then the emitted
+getters are heap dependent. For example, the value of place ``t2``
+depends on field ``self.int_field``::
+
+    write_int_io(t1, self.int_field, t2)
+
+This has interesting consequences such as:
+
+1.  Postcondition must have access to ``self.int_field``, otherwise
+    ``t2`` getter is not framed::
+
+        IOExists1(Place)(
+            lambda t2: (
+            Requires(
+                Acc(self.int_field) and
+                write_int_io(t1, self.int_field, t2)
+            ),
+            Ensures(
+                t2 == Result() # ERROR: not.wellformed:insufficient.permission
+            ),
+            )
+        )
+
+2.  Similarly, if defining getter is heap dependent and guarded by
+    conditional, other branch fails well-formedness check::
+
+        Requires(
+            token(t1) and
+            (
+                Acc(self.int_field1, 1/2) and
+                write_int_io(t1, self.int_field1, t2)
+            ) if b else (
+                Acc(self.int_field2, 1/2) and
+                write_int_io(t1, self.int_field2, t2)
+                               # ERROR: not.wellformed:insufficient.permission
+            )
+        ),
+
+3.  If defining getter is changed from heap independent in overridden
+    method to heap dependent in a overriding method and overriding
+    method takes all permission to the heap location, the behavioural
+    subtyping check fails because information about getter equality is
+    havocked.
+
+Currently, the plan is to ignore the problem because storing arguments
+in fields should not be too common in practise:
+
+1.  The Petri Net provided at the entry point cannot depend on the heap
+    – otherwise also permissions has to be provided at the entry
+    point, which does not make much sense.
+2.  It is not allowed to have permissions in non-basic IO operation
+    definitions.
+
+    .. todo::
+
+        Implement this check.
+
+.. todo::
+
+    Things to investigate:
+
+    1.  Does wrapping getters in ``old`` in postcondition solve the
+        issue of having to provide permissions to fields in
+        postcondition?
+
 """
 
 
