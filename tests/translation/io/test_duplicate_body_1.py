defd801171ab7eaa4fc059a9a2601423c74ee2e8
Vytautas Astrauskas <vakaras@users.noreply.github.com>
2016-07-06 20:25:58 +0200

    Added test for duplicate body.

---
+++ tests/translation/io/test_duplicate_body_1.py
@@ -0,0 +1,23 @@
+from py2viper_contracts.contracts import Requires, Predicate
+from py2viper_contracts.io import *
+
+
+@IOOperation
+def read_int_io(
+        t_pre: Place,
+        result: int = Result(),
+        t_post: Place = Result(),
+        ) -> bool:
+    Terminates(True)
+
+
+@IOOperation
+def read_int_io_twice(
+        t_pre: Place,
+        result: int = Result(),
+        t_post: Place = Result(),
+        ) -> bool:
+    Terminates(True)
+    return read_int_io(t_pre, result, t_post)
+    #:: ExpectedOutput(type.error:dead.code)
+    return read_int_io(t_pre, result, t_post)
