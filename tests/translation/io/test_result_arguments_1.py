c106710511c0f23b38ff013ae2072f28a045ec87
Vytautas Astrauskas <vastrauskas@gmail.com>
2016-07-07 18:52:43 +0200

    Added tests for using non-existential variables in IO operation result positions.

---
+++ tests/translation/io/test_result_arguments_1.py
@@ -0,0 +1,23 @@
+from py2viper_contracts.contracts import Requires
+from py2viper_contracts.io import *
+
+
+@IOOperation
+def read_int_io(
+        t_pre: Place,
+        result: int = Result(),
+        t_post: Place = Result(),
+        ) -> bool:
+    Terminates(True)
+
+
+def test(t1: Place) -> None:
+    IOExists2(Place, int)(
+        lambda t2, value: (
+        Requires(
+            token(t1) and
+            #:: ExpectedOutput(invalid.program:invalid.io_operation_use.not_variable_in_result_position)
+            read_int_io(t1, 2, t2)
+        ),
+        )
+    )
