425742592d92b697d86fa50024b5561da0f28317
Vytautas Astrauskas <vakaras@users.noreply.github.com>
2016-07-08 19:02:10 +0200

    Started creating stdio_simple library.

--- tests/verification/io/verifast/hello_world.py
+++ tests/verification/io/verifast/hello_world.py
@@ -1,41 +1,16 @@
 from py2viper_contracts.contracts import (
-    ContractOnly,
+    Import,
     Requires,
     Ensures,
 )
 from py2viper_contracts.io import *
-from typing import Tuple
 
-
-stdout = 1 # TODO: Use TextIO as soon as #42 or #43 is fixed.
-
-
-@IOOperation
-def write_char_io(
-        t_pre: Place,
-        #fp: TextIO,
-        fp: int,
-        c: str,
-        success: bool = Result(),
-        t_post: Place = Result()) -> bool:
-    Terminates(True)
-
-
-@ContractOnly
-def putchar(c: str, t1: Place) -> Tuple[bool, Place]:
-    IOExists2(Place, bool)(
-        lambda t2, success: (
-        Requires(
-            token(t1) and
-            write_char_io(t1, stdout, c, success, t2)
-        ),
-        Ensures(
-            token(t2) and
-            success == Result()[0] and
-            t2 == Result()[1]
-        )
-    ))
-
+from verifast.stdio_simple import (
+    stdout,
+    write_char_io,
+    putchar,
+)
+Import('stdio_simple.py')
 
 
 def main(t1: Place) -> Place:
