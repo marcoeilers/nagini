0afdd1a61e3e770a83ad09ff21133a7792acc21c
Vytautas Astrauskas <vastrauskas@gmail.com>
2016-07-12 15:58:12 +0200

    Added basic mypy stubs for Scala code.

---
+++ stubs/viper/silver/ast.py
@@ -0,0 +1,14 @@
+from typing import Any
+
+class Node:
+    pass
+
+class IdentifierPosition:
+    def file(self) -> Any:
+        pass
+    def start(self) -> Any:
+        pass
+    def end(self) -> Any:
+        pass
+    def id(self) -> str:
+        pass
