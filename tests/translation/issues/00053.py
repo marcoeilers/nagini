c230fae1cb4ea7f8149035370d5ec9b6ae2f6a99
Vytautas Astrauskas <vakaras@users.noreply.github.com>
2016-08-09 15:57:57 +0200

    Added regression test for issue 53.

---
+++ tests/translation/issues/00053.py
@@ -0,0 +1,16 @@
+from py2viper_contracts.contracts import (
+    Exsures,
+)
+
+
+def callee() -> int:
+    Exsures(Exception, True)
+    raise Exception()
+
+
+def caller() -> int:
+    try:
+        a = callee()
+    except:
+        return a
+    return 5
