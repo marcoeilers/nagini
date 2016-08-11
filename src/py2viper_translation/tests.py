65826a1e45675a700c9dd7040affbfe6266ac3fc
Vytautas Astrauskas <vastrauskas@gmail.com>
2016-08-11 16:38:14 +0200

    Added configuration option to skip a list of tests.

--- src/py2viper_translation/tests.py
+++ src/py2viper_translation/tests.py
@@ -242,7 +242,11 @@ def _test_files(test_dir):
         for file_name in file_names:
             if file_name.endswith('.py'):
                 result.append(join(root, file_name))
-    return sorted(result)
+    result = [
+        path
+        for path in sorted(result)
+        if path not in config.test_config.ignore_tests]
+    return result
 
 
 def verification_test_files():
