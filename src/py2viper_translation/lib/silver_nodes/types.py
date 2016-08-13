bfdf82442cd9090c1b05970b87bd1b2ecb7c0881
Vytautas Astrauskas <vastrauskas@gmail.com>
2016-08-13 17:32:59 +0200

    Refactoring Measure implementation.

--- src/py2viper_translation/lib/silver_nodes/types.py
+++ src/py2viper_translation/lib/silver_nodes/types.py
@@ -3,8 +3,12 @@
 
 import abc
 
+from typing import List
+
 from py2viper_translation.lib.typedefs import (
     Expr,
+    Position,
+    Info,
 )
 
 
@@ -61,7 +65,69 @@ class DomainType(Type):
     """A domain type."""
 
     def __init__(self, name: str) -> None:
-        self._name = name
+        self.name = name
 
     def translate(self, translator: 'AbstractTranslator') -> Expr:
-        return translator.viper.DomainType(self._name, {}, [])
+        return translator.viper.DomainType(self.name, {}, [])
+
+
+class DomainFuncApp:
+    """A domain function call."""
+
+    def __init__(
+            self, name: str, args: List['Expression'],
+            domain: DomainType) -> None:
+        self._name = name
+        self._args = args
+        self._domain = domain
+
+    def translate(self, translator: 'AbstractTranslator', ctx: 'Context',
+                  position: Position, info: Info) -> Expr:
+        """Translate to Silver DomainFuncApp."""
+        args = [arg.translate(translator, ctx, position, info)
+                for arg in self._args]
+        domain = self._domain.translate(translator)
+        return translator.viper.DomainFuncApp(
+            self._name, args, {}, domain, args, position, info,
+            self._domain.name)
+
+
+class Domain:
+    """A helper class for using a domain defined in Silver."""
+
+    def __init__(self, name: str) -> None:
+        self._name = name
+        self._functions = set()
+
+    def get_type(self) -> DomainType:
+        """Return the Domain Type."""
+        return DomainType(self._name)
+
+    def declare_function(self, name: str) -> None:
+        """Declare new function in domain."""
+        self._functions.add(name)
+
+    def call_function(
+            self, name: str, args: List['Expression']) -> DomainFuncApp:
+        """Call a domain function."""
+        assert name in self._functions
+        return DomainFuncApp(name, args, self.get_type())
+
+
+class Sequence:
+    """A helper class for generating Silver sequences."""
+
+    def __init__(self, typ: Type, elements: List['Expression']) -> None:
+        self._type = typ
+        self._elements = elements
+
+    def translate(self, translator: 'AbstractTranslator', ctx: 'Context',
+                  position: Position, info: Info) -> Expr:
+        """Translate to Silver sequence."""
+        if not self._elements:
+            typ = self._type.translate(translator)
+            return translator.viper.EmptySeq(typ, position, info)
+        else:
+            elements = [element.translate(translator, ctx, position, info)
+                        for element in self._elements]
+            return translator.viper.ExplicitSeq(elements, position, info)
--- src/py2viper_translation/translators/obligation/measures.py
+++ src/py2viper_translation/translators/obligation/measures.py
@@ -10,13 +10,7 @@ from py2viper_translation.lib.program_nodes import (
     PythonVar,
 )
 from py2viper_translation.lib.typedefs import (
-    Info,
-    Expr,
-    Position,
     Stmt,
-)
-from py2viper_translation.translators.obligation.types.base import (
-    ObligationInstance,
 )
 
 
@@ -25,6 +19,18 @@ class MeasureMap:
 
     def __init__(self, measure_map_var: PythonVar) -> None:
         self._map_var = measure_map_var
+        self._measure_domain = sil.Domain('Measure$')
+        self._measure_domain.declare_function('Measure$create')
+
+    @property
+    def _measure_var_type(self) -> sil.Type:
+        return sil.SeqType(self._measure_domain.get_type())
+
+    def _create_measure(
+            self, guard: sil.BoolExpression, key: sil.RefExpression,
+            value: sil.IntExpression) -> sil.DomainFuncApp:
+        return self._measure_domain.call_function(
+            'Measure$create', [guard, key, value])
 
     def check(self, reference: sil.RefExpression,
               value: sil.IntExpression) -> sil.BoolExpression:
@@ -33,9 +39,8 @@ class MeasureMap:
                 obligation_config.disable_measure_check):
             return sil.TrueLit()
         args = [
-            sil.CallArg(
-                'map', sil.SeqType(sil.DomainType('Measure$')),
-                sil.VarRef(self._map_var)),
+            sil.CallArg('map', self._measure_var_type,
+                        sil.VarRef(self._map_var)),
             sil.CallArg('key', sil.REF, reference),
             sil.CallArg('value', sil.INT, value),
         ]
@@ -56,53 +61,18 @@ class MeasureMap:
         """
         position = translator.no_position(ctx)
         info = translator.no_info(ctx)
-        statements = []
-        typ = translator.viper.DomainType('Measure$', {}, [])
-        init = translator.viper.LocalVarAssign(
-            self._map_var.ref(),
-            translator.viper.EmptySeq(typ, position, info),
-            position, info)
-        statements.append(init)
+        measures = []
         for instance in obligation_instances:
             guard_expression = instance.create_guard_expression()
-            call = self._create_measure_set_call(
-                instance.obligation_instance, position, info,
-                translator, ctx, overriding)
-            if guard_expression.is_empty():
-                statement = call
-            else:
-                condition = guard_expression.translate(
-                    translator, ctx, position, info)
-                statement = translator.viper.If(
-                    condition, call,
-                    translator.translate_block([], position, info),
-                    position, info)
-            statements.append(statement)
-        return statements
-
-    def _create_measure_set_call(
-            self, obligation_instance: ObligationInstance,
-            position: Position, info: Info,
-            translator: 'AbstractTranslator', ctx: Context,
-            overriding: bool) -> Expr:
-        reference = obligation_instance.get_target()
-        reference_expr = reference.translate(translator, ctx, position, info)
-        measure = obligation_instance.get_measure()
-        if overriding:
-            measure = sil.Inc(measure)
-        measure_expr = measure.translate(translator, ctx, position, info)
-
-        typ = translator.viper.DomainType('Measure$', {}, [])
-        args = [reference_expr, measure_expr]
-        elems = [
-            translator.viper.DomainFuncApp(
-                'Measure$create', args, {}, typ, args, position, info,
-                'Measure$')
-        ]
-        seq = translator.viper.ExplicitSeq(elems, position, info)
-
-        return translator.viper.LocalVarAssign(
-            self._map_var.ref(),
-            translator.viper.SeqAppend(
-                self._map_var.ref(), seq, position, info),
-            position, info)
+            value = instance.obligation_instance.get_measure()
+            if overriding:
+                value = sil.Inc(value)
+            measure = self._create_measure(
+                guard_expression,
+                instance.obligation_instance.get_target(), value)
+            measures.append(measure)
+        assign = sil.Assign(
+            self._map_var,
+            sil.Sequence(self._measure_domain.get_type(), measures))
+        statement = assign.translate(translator, ctx, position, info)
+        return [statement]
