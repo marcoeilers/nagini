8229c15190827fd4f8b8d5728fb774f5e262c96e
Vytautas Astrauskas <vakaras@users.noreply.github.com>
2016-08-16 15:08:24 +0200

    Added missing annotations for regression test 57.

--- tests/verification/issues/00057.py
+++ tests/verification/issues/00057.py
@@ -12,8 +12,15 @@ class C:
 
 
 def callee(b: B, c: C) -> None:
+    #:: ExpectedOutput(assert.failed:assertion.false)|MissingOutput(assert.failed:assertion.false, /py2viper/issue/57/)
     Assert(b is not c)
 
 
 def test() -> None:
     callee(None, None)
+
+
+def test2() -> None:
+    Assert(None is None)
+    #:: ExpectedOutput(assert.failed:assertion.false)
+    Assert(None is not None)
