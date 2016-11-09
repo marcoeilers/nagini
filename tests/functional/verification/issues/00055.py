66ded7ad2ec721fdf639aaa43d04a7eb230e18db
Marco Eilers <marcoeilers@users.noreply.github.com>
2016-11-09 16:16:40 +0100

    Enabled test for issue 55

--- tests/functional/verification/issues/00055.py
+++ tests/functional/verification/issues/00055.py
@@ -1,15 +1,22 @@
-#:: IgnoreFile(/py2viper/issue/55/)
 from py2viper_contracts.contracts import (
     Assert,
 )
 
 
+class Exc1(Exception):
+    pass
+
+
+class Exc2(Exception):
+    pass
+
+
 def test() -> None:
     try:
         x = 5
-    except OSError as ex1:
+    except Exc1 as ex1:
         x = 6
         try:
             x = 7
-        except OverflowError as ex2:
-            Assert(isinstance(ex1, OSError))
+        except Exc2 as ex2:
+            Assert(isinstance(ex1, Exc2))
