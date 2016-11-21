6818a9d6998043bc0eeee9055c5ee2eea22387ff
Marco Eilers <marcoeilers@users.noreply.github.com>
2016-11-21 22:57:05 +0100

    Fixed typo because I'm stupid

--- src/py2viper_translation/lib/program_nodes.py
+++ src/py2viper_translation/lib/program_nodes.py
@@ -510,7 +510,7 @@ class OptionalType(UnionType):
     def __init__(self, typ: PythonType) -> None:
         super().__init__([typ])
         self.type_args = [None, typ]
-        self.optional_type = type
+        self.optional_type = typ
 
 
 class MethodType(Enum):
