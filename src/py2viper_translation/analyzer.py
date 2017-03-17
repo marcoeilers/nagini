2c7769f4f6bf5ac34948ea3c7048a920a13ea59b
Sam Hitz <samuel.hitz@inf.ethz.ch>
2017-03-17 13:02:41 +0100

    addressed comments

--- src/py2viper_translation/analyzer.py
+++ src/py2viper_translation/analyzer.py
@@ -228,29 +228,32 @@ class Analyzer(ast.NodeVisitor):
             cls.interface = True
             cls.defined = True
         for class_name in interface:
-            cls = self.find_or_create_class(class_name)
-            if_cls = interface[class_name]
-            if 'type_vars' in if_cls:
-                for i in range(if_cls['type_vars']):
-                    name = 'var' + str(i)
-                    cls.type_vars[name] = TypeVar(name, cls, None, i, None, [],
-                                                  None)
-            if 'extends' in if_cls:
-                superclass = self.find_or_create_class(if_cls['extends'],
-                    module=self.module.global_module)
-                cls.superclass = superclass
-            for method_name in if_cls.get('methods', []):
-                if_method = if_cls['methods'][method_name]
-                self._add_native_silver_method(method_name, if_method, cls,
-                                               False)
-            for method_name in if_cls.get('functions', []):
-                if_method = if_cls['functions'][method_name]
-                self._add_native_silver_method(method_name, if_method, cls,
-                                               True)
-            for pred_name in if_cls.get('predicates', []):
-                if_pred = if_cls['predicates'][pred_name]
-                self._add_native_silver_method(pred_name, if_pred, cls,
-                                               True, True)
+            self._process_interface_class(class_name, interface[class_name])
+
+    def _process_interface_class(self, class_name: str, if_cls: Dict,
+                                 node_factory: ProgramNodeFactory = None):
+        cls = self.find_or_create_class(class_name)
+        if 'type_vars' in if_cls:
+            for i in range(if_cls['type_vars']):
+                name = 'var' + str(i)
+                cls.type_vars[name] = TypeVar(
+                    name, cls, None, i, None, [], None)
+        if 'extends' in if_cls:
+            superclass = self.find_or_create_class(
+                if_cls['extends'], module=self.module.global_module)
+            cls.superclass = superclass
+        for method_name in if_cls.get('methods', []):
+            if_method = if_cls['methods'][method_name]
+            self._add_native_silver_method(
+                method_name, if_method, cls, False, node_factory=node_factory)
+        for method_name in if_cls.get('functions', []):
+            if_method = if_cls['functions'][method_name]
+            self._add_native_silver_method(
+                method_name, if_method, cls, True, node_factory=node_factory)
+        for pred_name in if_cls.get('predicates', []):
+            if_pred = if_cls['predicates'][pred_name]
+            self._add_native_silver_method(
+                pred_name, if_pred, cls, True, True, node_factory=node_factory)
 
     def _add_native_silver_method(
             self, method_name: str, if_method: Dict[str, Any], cls: PythonClass,
