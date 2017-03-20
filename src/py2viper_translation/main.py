c03d10615fe670d482cacbcc60f29a8bc862b176
Marco Eilers <marcoeilers@users.noreply.github.com>
2017-03-20 19:38:14 +0100

    Merge fixing

--- src/py2viper_translation/main.py
+++ src/py2viper_translation/main.py
@@ -19,7 +19,7 @@ from py2viper_translation.lib.jvmaccess import JVM
 from py2viper_translation.lib.typeinfo import TypeException, TypeInfo
 from py2viper_translation.lib.util import InvalidProgramException
 from py2viper_translation.lib.viper_ast import ViperAST
-from py2viper_translation.sif.lib.program_nodes import SIFAnalyzer
+from py2viper_translation.sif_analyzer import SIFAnalyzer
 from py2viper_translation.sif_translator import SIFTranslator
 from py2viper_translation.translator import Translator
 from py2viper_translation.verifier import (
