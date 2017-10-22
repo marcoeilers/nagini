67539a1c97bd9038aa1c0a853102b2cef0368dce
Ben Weber <dev.ben.weber@gmail.com>
2017-10-22 23:48:11 +0200

    Added pure, while loop and n times examples

---
+++ tests/closures/verification/examples/n_times.py
@@ -0,0 +1,132 @@
+from typing import Callable, Optional
+from nagini_contracts.contracts import (
+    Implies,
+    Requires,
+    Ensures,
+    Result,
+    Old,
+    Invariant,
+    Predicate,
+    Fold,
+    Unfold,
+    Unfolding,
+    Pure,
+    Acc,
+    CallSlot,
+    CallSlotProof,
+    UniversallyQuantified,
+    ClosureCall,
+    Assert,
+)
+
+
+class State:
+
+    def __init__(
+        self,
+        counter: int,
+        value: int,
+        offset: int,
+        next: Optional['State']
+    ) -> None:
+
+        self.counter = counter
+        self.value = value
+        self.offset = offset
+        self.next = next
+        Ensures(Acc(self.counter) and Acc(self.value))
+        Ensures(Acc(self.offset) and Acc(self.next))
+        Ensures(self.counter == counter and self.value == value)
+        Ensures(self.offset == offset and self.next == next)
+
+
+f_type = Callable[[State], Optional[object]]
+
+
+@Predicate
+def n_inv(s: State, i: int, n_inv_token: int) -> bool:
+    return (
+            Acc(s.counter) and Acc(s.value) and
+            s.counter == i and s.value == i * (i + 1) // 2
+        if n_inv_token == 1 else
+        True
+    )
+
+
+@CallSlot
+def n_times_slot(f: f_type, n_inv_token: int) -> None:
+
+    @UniversallyQuantified
+    def uq(s: State, i: int) -> None:
+        Requires(n_inv(s, i, n_inv_token))
+
+        f(s)
+
+        Ensures(n_inv(s, i + 1, n_inv_token))
+
+
+def n_times(f: f_type, n: int, s: State, n_inv_token: int) -> None:
+    Requires(0 <= n)
+    Requires(n_inv(s, 0, n_inv_token))
+    Requires(n_times_slot(f, n_inv_token))
+    Ensures(n_inv(s, n, n_inv_token))
+
+    i = 0
+    while i < n:
+        Invariant(i <= n)
+        Invariant(n_inv(s, i, n_inv_token))
+
+        ClosureCall(f(s), n_times_slot(f, n_inv_token)(s, i))
+        i += 1
+
+    # FIXME: this shouldn't be necessary
+    # (would be impossible with proper 'parametric assertions')
+    # However while i == n, theyr values as Ref types seem to be uneqal
+    Unfold(n_inv(s, i, n_inv_token))
+    Fold(n_inv(s, n, n_inv_token))
+
+
+def sum_range(s: State) -> Optional[object]:
+    Requires(Acc(s.counter) and Acc(s.value))
+    Ensures(Acc(s.counter) and Acc(s.value))
+    Ensures(s.counter == Old(s.counter) + 1)
+    Ensures(s.value == Old(s.value + s.counter + 1))
+
+    s.counter += 1
+    s.value += s.counter
+
+    return None
+
+
+def n_times_client() -> None:
+
+    s = State(0, 0, 0, None)
+
+    f = sum_range
+
+    @CallSlotProof(n_times_slot(f, 1))
+    def n_times_slot(f: f_type, n_inv_token: int) -> None:
+
+        @UniversallyQuantified
+        def uq(s: State, i: int) -> None:
+            Requires(n_inv(s, i, n_inv_token))
+            Unfold(n_inv(s, i, n_inv_token))
+
+            assert i == s.counter
+            assert s.value == i * (i + 1) // 2
+            ClosureCall(f(s), sum_range)
+
+            assert s.counter == i + 1
+            assert s.value == i * (i + 1) // 2 + i + 1
+            assert s.value == i * (i + 1) // 2 + i + 1
+            assert s.value == i * (i + 1) // 2 + 2 * (i + 1) // 2
+            assert s.value == (i * (i + 1) + 2 * (i + 1)) // 2
+
+            Fold(n_inv(s, i + 1, n_inv_token))
+            Ensures(n_inv(s, i + 1, n_inv_token))
+
+    Fold(n_inv(s, 0, 1))
+    n_times(f, 23, s, 1)
+    Unfold(n_inv(s, 23, 1))
+    assert s.value == 276
+    assert s.counter == 23
---
+++ tests/closures/verification/examples/pure.py
@@ -0,0 +1,141 @@
+from typing import Callable
+from nagini_contracts.contracts import (
+    CallSlot,
+    CallSlotProof,
+    UniversallyQuantified,
+    ClosureCall,
+    Pure,
+    Result,
+    Requires,
+    Ensures,
+    Acc
+)
+
+
+def choice() -> bool:
+    return True
+
+
+class Argument:
+
+    def __init__(self, value_a: int, value_b: int) -> None:
+        self.value_a = value_a
+        self.value_b = value_b
+        Ensures(Acc(self.value_a) and Acc(self.value_b))
+        Ensures(self.value_a == value_a and self.value_b == value_b)
+
+
+f_type = Callable[[Argument, int], int]
+
+
+@Pure
+def add(arg: Argument, x: int) -> int:
+    Requires(Acc(arg.value_a))
+    Ensures(Result() == arg.value_a + x)
+    return arg.value_a + x
+
+
+@Pure
+def mul(arg: Argument, x: int) -> int:
+    Requires(Acc(arg.value_b))
+    Ensures(Result() == arg.value_b * x)
+    return arg.value_b * x
+
+
+@Pure
+@CallSlot
+def add_or_mul(f: f_type) -> None:
+
+        @UniversallyQuantified
+        def uq(arg: Argument, x: int) -> None:
+            Requires(Acc(arg.value_a) and Acc(arg.value_b))
+
+            y = f(arg, x)
+
+            Ensures(y == arg.value_a + x or y == arg.value_b * x)
+
+
+@CallSlot
+def hof_slot(f: f_type) -> None:
+
+        @UniversallyQuantified
+        def uq(arg: Argument, x: int) -> None:
+            Requires(Acc(arg.value_a, 1 / 2) and Acc(arg.value_b, 1 / 3))
+
+            y = f(arg, x)
+
+            Ensures(Acc(arg.value_a, 1 / 2) and Acc(arg.value_b, 1 / 3))
+            Ensures(y <= arg.value_a + x or y >= arg.value_b * x)
+
+
+def hof(f: f_type, arg: Argument) -> int:
+    Requires(Acc(arg.value_a, 1 / 2) and Acc(arg.value_b, 1 / 3))
+    Requires(hof_slot(f))
+
+    Ensures(Acc(arg.value_a, 1 / 2) and Acc(arg.value_b, 1 / 3))
+    Ensures(Result() <= arg.value_a + 5 or Result() >= arg.value_b * 5)
+
+    return ClosureCall(f(arg, 5), hof_slot(f)(arg, 5))
+
+
+def client() -> None:
+
+    arg = Argument(1, 2)
+    assert arg.value_a == 1
+    assert arg.value_b == 2
+
+    f = add
+    y = ClosureCall(f(arg, 3), add)  # type: int
+    assert y == 4
+    assert arg.value_a == 1
+    assert arg.value_b == 2
+
+    f = mul
+    y = ClosureCall(f(arg, 3), mul)
+    assert y == 6
+    assert arg.value_a == 1
+    assert arg.value_b == 2
+
+    if choice():
+        f = add
+    else:
+        f = mul
+
+    @CallSlotProof(add_or_mul(f))
+    def add_or_mul_proof(f: f_type) -> None:
+
+            @UniversallyQuantified
+            def uq(arg: Argument, x: int) -> None:
+                Requires(Acc(arg.value_a) and Acc(arg.value_b))
+
+                if f == add:
+                    y = ClosureCall(f(arg, x), add)  # type: int
+                else:
+                    y = ClosureCall(f(arg, x), mul)
+
+                Ensures(y == arg.value_a + x or y == arg.value_b * x)
+
+    y = ClosureCall(f(arg, 3), add_or_mul(f)(arg, 3))
+    assert y == 4 or y == 6
+    assert arg.value_a == 1
+    assert arg.value_b == 2
+
+    @CallSlotProof(hof_slot(f))
+    def hof_slot_proof(f: f_type) -> None:
+
+            @UniversallyQuantified
+            def uq(arg: Argument, x: int) -> None:
+                Requires(Acc(arg.value_a) and Acc(arg.value_b))
+
+                if f == add:
+                    y = ClosureCall(f(arg, x), add)  # type: int
+                else:
+                    y = ClosureCall(f(arg, x), mul)
+
+                Ensures(y <= arg.value_a + x or y >= arg.value_b * x)
+
+    h = hof
+    y = ClosureCall(h(f, arg), hof)
+    assert y <= 6 or y >= 10
+    assert arg.value_a == 1
+    assert arg.value_b == 2
