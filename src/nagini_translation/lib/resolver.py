30f6cacdab0d15042883daa1cb8e0db2f87e4da9
Fábio Pakk Selmi-Dei <fabio.pakk@inf.ethz.ch>
2017-11-20 16:44:57 +0100

    Changed code to comply with formatting standards

--- src/nagini_translation/lib/resolver.py
+++ src/nagini_translation/lib/resolver.py
@@ -372,8 +372,10 @@ def _get_call_type(node: ast.Call, module: PythonModule,
             return target.type
     elif isinstance(node.func, ast.Attribute):
         rectype = get_type(node.func.value, containers, container)
-        if isinstance(node.func.value, ast.Name) and isinstance(rectype, UnionType):
-            set_of_types = {type.get_func_or_method(node.func.attr).type for type in rectype.type_args}
+        if (isinstance(node.func.value, ast.Name) and
+            isinstance(rectype, UnionType)):
+            set_of_types = {type.get_func_or_method(node.func.attr).type
+                for type in rectype.type_args}
             if len(set_of_types) == 1:
                 return set_of_types.pop()
             else:
