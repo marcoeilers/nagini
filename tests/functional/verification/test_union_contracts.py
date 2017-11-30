2380d2678ce8b78c9e275ca317217c5018c16549
Fábio Pakk Selmi-Dei <fabio.pakk@inf.ethz.ch>
2017-11-30 10:03:24 +0100

    Added more test cases for union type

--- tests/functional/verification/test_union_contracts.py
+++ tests/functional/verification/test_union_contracts.py
@@ -16,24 +16,30 @@ class B:
 class C:
     def foo(self, i: int) -> str:
         Requires(True)
-        Ensures(Result() == '4')
-        return '4'
+        Ensures(Result() == '5')
+        return '5'
 
-def a(o: Union[A, B]) -> None:
+class D(A):
+    def foo(self, i: int) -> int:
+        Requires(True)
+        Ensures(Result() == 8)
+        return 8
+
+def test_1(o: Union[A, B]) -> None:
     x = o.foo(5)
 
-def b(o: Union[A, B, C]) -> None:
+def test_2(o: Union[A, B, C]) -> None:
     x = o.foo(5)
 
-def c(o: Union[A, B]) -> None:
+def test_3(o: Union[A, B]) -> None:
     #:: ExpectedOutput(call.precondition:assertion.false)
     x = o.foo(-5)
 
-def d(o: Union[A, B, C]) -> None:
+def test_4(o: Union[A, B, C]) -> None:
     #:: ExpectedOutput(call.precondition:assertion.false)
     x = o.foo(-5)
 
-def e(o: Union[A, B]) -> None:
+def test_5(o: Union[A, B]) -> None:
     if isinstance(o, A):
         x = o.foo(-2)
         assert x == 4
@@ -43,34 +49,37 @@ def e(o: Union[A, B]) -> None:
         #:: ExpectedOutput(call.precondition:assertion.false)
         x = o.foo(-34)
 
-def f(o: Union[A, B], i: int) -> None:
+def test_6(o: Union[A, B], i: int) -> None:
     if isinstance(o, B):
         #:: ExpectedOutput(call.precondition:assertion.false)
         x = o.foo(i) 
 
-def g(o: Union[A, B], i: int) -> None:
+def test_7(o: Union[A, B], i: int) -> None:
     #:: ExpectedOutput(call.precondition:assertion.false)
     x = o.foo(i)
 
-def h(o: Union[A, B], i: int) -> None:
+def test_8(o: Union[A, B], i: int) -> None:
     if i > 0:
         x = o.foo(i)
         assert x == 4 or x == 6
 
-def i(o: Union[A, B], j: int) -> None:
+def test_9(o: Union[A, B], j: int) -> None:
     Requires(j > 0)
     o.foo(j)
 
-def j(o: Union[A, B], i: int) -> int:
+def test_10(o: Union[A, B], i: int) -> int:
     Requires(i > 0)
     Ensures(Result() == 4 or Result() == 6)
     return o.foo(i)
 
-def k(o: Union[A, Union[B, C]]) -> None:
+def test_11(o: Union[A, Union[B, C]]) -> None:
     x = o.foo(5)
 
-def l(o: Union[Union[B, C]]) -> None:
+def test_12(o: Union[Union[B, C]]) -> None:
     x = o.foo(5)
 
-def m(o: Union[A]) -> None:
+def test_13(o: Union[Union[A, B], C]) -> None:
+    x = o.foo(5)
+
+def test_14(o: Union[A]) -> None:
     x = o.foo(5)
