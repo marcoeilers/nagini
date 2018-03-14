59217de1734c69c7b72471942962bf76d8e5621a
Fábio Pakk Selmi-Dei <fabio.pakk@inf.ethz.ch>
2018-03-14 15:27:15 +0100

    Added ADT documentation and work around MyPy limitation with TupleTypes (MyPy issue 979).

--- src/nagini_translation/lib/typeinfo.py
+++ src/nagini_translation/lib/typeinfo.py
@@ -66,6 +66,8 @@ class TypeVisitor(mypy.traverser.TraverserVisitor):
                 not isinstance(rectype, mypy.types.AnyType) and
                 not isinstance(rectype, mypy.types.UnionType) and
                 not isinstance(rectype, mypy.types.TypeVarType)):
+            if not hasattr(rectype, 'type'):
+                rectype = rectype.fallback
             self.set_type(rectype.type.fullname().split('.') + [node.name],
                           self.type_of(node),
                           node.line, col(node))
