433c2454a5f96a0903720edd60b13eb74bb7e37b
Fábio Pakk Selmi-Dei <fabio.pakk@inf.ethz.ch>
2018-05-17 13:43:02 +0200

    Added license header to new file.

--- src/nagini_contracts/adt.py
+++ src/nagini_contracts/adt.py
@@ -1,6 +1,9 @@
-#
-#
-#
+"""
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""
+
 # The ADT module provides support for specifying algebraic data types for Nagini.
 # ADTs are meant to be used exclusively in contracts: although they are syntatically
 # correct Python constructs, they should be defined with no functional intent while
