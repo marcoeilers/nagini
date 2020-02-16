3253733d7d1b1ea84d89092cbb4b9216e36a0cb7
Marco Eilers <eilers.marco@googlemail.com>
2020-02-16 03:01:22 +0100

    Ignoring non-terminating ARP tests

--- tests/arp/verification/test_acc_func.py
+++ tests/arp/verification/test_acc_func.py
@@ -1,6 +1,8 @@
 # Any copyright is dedicated to the Public Domain.
 # http://creativecommons.org/publicdomain/zero/1.0/
 
+#:: IgnoreFile(carbon)(228)
+
 from nagini_contracts.contracts import *
 
 
