af2eea01a122381622f6c60ba1243731af56dce6
Marco Eilers <eilers.marco@googlemail.com>
2020-02-25 13:49:18 +0100

    Ignoring more non-terminating tests

--- tests/arp/verification/test_arp_read_write.py
+++ tests/arp/verification/test_arp_read_write.py
@@ -1,6 +1,10 @@
 # Any copyright is dedicated to the Public Domain.
 # http://creativecommons.org/publicdomain/zero/1.0/
 
+
+#:: IgnoreFile(carbon)(228)
+
+
 from nagini_contracts.lock import Lock
 from nagini_contracts.contracts import *
 from nagini_contracts.obligations import Level, WaitLevel, MustTerminate
