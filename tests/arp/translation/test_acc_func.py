0a2aa83fd4cd5d9cc45ea9d5b75d9c9693f66b1b
Marco Eilers <eilers.marco@googlemail.com>
2020-02-27 17:18:33 +0100

    Also ignoring ARP translation test

--- tests/arp/translation/test_acc_func.py
+++ tests/arp/translation/test_acc_func.py
@@ -1,6 +1,10 @@
 # Any copyright is dedicated to the Public Domain.
 # http://creativecommons.org/publicdomain/zero/1.0/
 
+
+#:: IgnoreFile(carbon)(228)
+
+
 from nagini_contracts.contracts import *
 
 
