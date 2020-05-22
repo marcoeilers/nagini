2e50e599dd10e5d90a3b073b59de4bcb82e6606b
Marco Eilers <eilers.marco@googlemail.com>
2020-05-22 23:09:02 +0200

    Fixed missing domain definition

--- src/nagini_translation/translators/type.py
+++ src/nagini_translation/translators/type.py
@@ -56,6 +56,7 @@ class TypeTranslator(CommonTranslator):
         if isinstance(cls, SilverType):
             return cls.type
         elif cls.name == CALLABLE_TYPE:
+            ctx.are_function_constants_used = True
             return self.viper.function_domain_type()
         elif cls.name in PRIMITIVES:
             cls = cls.try_box()
