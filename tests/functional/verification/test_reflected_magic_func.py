8f241728bb992a8b2a68d04cff3dc1201fa0f9cf
Pascal Devenoge <dpascal@ethz.ch>
2024-04-26 11:06:58 +0200

    Add expected failure tests to test_reflected_magic_func.py

--- tests/functional/verification/test_reflected_magic_func.py
+++ tests/functional/verification/test_reflected_magic_func.py
@@ -10,8 +10,9 @@ class A():
 
 class B():
   pass
-  
-r1 = A() + B()
+
+def basic_add_test() -> None:  
+  r1 = A() + B()  
 
 
 # __add__ of left operand takes priority
@@ -23,8 +24,9 @@ class D():
   def __add__(self, other: 'C') -> 'D':
     Requires(False)
     assert False
-  
-r2 = C() + D()
+
+def basic_add_test_2() -> None:  
+  r2 = C() + D()
 
 
 # __add__ prefered over __radd__
@@ -35,8 +37,9 @@ class E():
   def __radd__(self, other: 'E') -> 'E':
     Requires(False)
     assert False
-  
-r3 = E() + E()
+
+def add_and_radd_test() -> None:  
+  r3 = E() + E()
 
 
 # Left operand takes precedence over right operand
@@ -57,7 +60,8 @@ class G():
     Requires(False)
     assert False
 
-r4 = F() + G()
+def left_operand_precedence_test() -> None:
+  r4 = F() + G()
 
 
 # If left operand has no matching __add__, check for __radd__ on right operand
@@ -67,8 +71,9 @@ class H():
 class I():
   def __radd__(self, other: 'H') -> 'I':
     return I()
-  
-r5 = H() + I()
+
+def basic_radd_test() -> None:  
+  r5 = H() + I()
 
 
 class F1():
@@ -80,7 +85,8 @@ class G1():
   def __radd__(self, other: 'F1') -> 'F1':
     return F1()
 
-r41 = F1() + G1()
+def basic_radd_test_2() -> None:
+  r41 = F1() + G1()
 
 
 class H1():
@@ -91,8 +97,9 @@ class H1():
 class I1(H1):
   def __radd__(self, other: 'H1') -> 'H1':
     return I1()
-  
-r51 = H1() + I1()
+
+def incompatible_add_test() -> None:  
+  r51 = H1() + I1()
 
 
 ############################################
@@ -112,7 +119,8 @@ class J():
 class K(J):
   pass
 
-r6 = 2 + K()
+def basic_subclass_test() -> None:
+  r6 = 2 + K()
 
 class L():
   def __add__(self, other: int) -> 'L':
@@ -124,8 +132,8 @@ class L():
   
 class M(L):
   pass
-
-r7 = M() + 2
+def basic_subclass_test_2() -> None:
+  r7 = M() + 2
 
 class P():
   def __add__(self, other: 'P') -> 'P':
@@ -138,7 +146,8 @@ class P():
 class Q(P):
   pass
 
-r9 = P() + Q()
+def basic_subclass_test_3() -> None:
+  r9 = P() + Q()
 
 class R():
   pass
@@ -147,7 +156,8 @@ class S(R):
   def __radd__(self, other: 'R') -> 'R':
     return R()
 
-r10 = R() + S()
+def basic_subclass_test_4() -> None:
+  r10 = R() + S()
 
 
 # If right operand is subtype of left operand, and
@@ -167,7 +177,8 @@ class O(N):
   def __radd__(self, other: 'N') -> 'N':
     return O()
   
-r8 = N() + O()
+def overridden_radd_test() -> None:
+  r8 = N() + O()
 
 class T():
   pass
@@ -181,7 +192,8 @@ class V(U):
   def __radd__(self: 'V', other: 'T') -> 'V':
     return self
 
-r11 = U() + V()
+def overridden_radd_test_2() -> None:
+  r11 = U() + V()
 
 class W():
   pass
@@ -199,4 +211,68 @@ class Y(X):
   def __radd__(self: 'Y', other: 'W') -> 'Y':
     return self
 
-r12 = X() + Y()
+def overridden_radd_test_3() -> None:
+  r12 = X() + Y()
+
+
+class IntContainer():
+  def __init__(self, i: int) -> None:
+    self.i = i
+    Ensures(Acc(self.i) and self.i == i)
+
+  def __add__(self, other: 'IntContainer') -> 'IntContainer':
+    Requires(Acc(self.i, 1/2) and Acc(other.i, 1/2))
+    Ensures(Acc(self.i, 1/2) and Acc(other.i, 1/2))
+    Ensures(Acc(Result().i) and Result().i == self.i + other.i)
+    return IntContainer(self.i + other.i)
+  
+
+def failure() -> None:
+  a = IntContainer(42)
+  b = IntContainer(15)
+  c = a + b
+
+  assert c.i == 57
+  #:: ExpectedOutput(assert.failed:assertion.false)
+  assert c.i == 42
+
+class Failure():
+  def __add__(self, other: 'Failure') -> 'Failure':
+    #:: ExpectedOutput(assert.failed:assertion.false)
+    assert False
+
+def failure_2() -> None:
+  a = Failure() + Failure()
+
+
+class Z():
+  def __init__(self, i: int) -> None:
+    self.i = i
+    Ensures(Acc(self.i) and self.i == i)
+
+  def __add__(self, other: 'Z') -> 'Z':
+    Requires(Acc(self.i, 1/2) and Acc(other.i, 1/2))
+    Ensures(Acc(self.i, 1/2) and Acc(other.i, 1/2))
+    Ensures(Acc(Result().i))
+    return Z(0)
+
+  def __radd__(self, other: 'Z') -> 'Z':
+    Requires(Acc(self.i, 1/2) and Acc(other.i, 1/2))
+    Ensures(Acc(self.i, 1/2) and Acc(other.i, 1/2))
+    Ensures(Acc(Result().i))
+    return Z(0)
+
+class ZZ(Z):
+  def __radd__(self, other: Z) -> 'ZZ':
+    Requires(Acc(self.i, 1/2) and Acc(other.i, 1/2))
+    Ensures(Acc(self.i, 1/2) and Acc(other.i, 1/2))
+    Ensures(Acc(Result().i) and Result().i == self.i + other.i)
+    return ZZ(self.i + other.i)
+
+def failure_3() -> None:
+  a = Z(42) + ZZ(42)
+
+  assert a.i == 84
+  #:: ExpectedOutput(assert.failed:assertion.false)
+  assert a.i == 0
+  
