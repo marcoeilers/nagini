991c6c8c37022842b6fee8632a25e7b25f22231b
Marco Eilers <eilers.marco@googlemail.com>
2024-10-23 23:03:04 +0200

    Changes for Python 3.12

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
             'mypy==0.900',
             'typed-ast==1.5.4',
             'toposort==1.5',
-            'jpype1==1.4.1',
+            'jpype1==1.5.0',
             'astunparse==1.6.2',
             'pytest==7.0.0',
             'z3-solver==4.8.7.0'
