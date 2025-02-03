b01efbdd64a90251f0f18564bea7183305fb9626
etiennebirling <etienne.birling@gmail.com>
2025-02-03 13:43:12 +0100

    Added more interesting feature to the test case

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from nagini_contracts.contracts import *
 from typing import  List
+class mycoolclass:
+    def __init__(self, arg:int )->None:
+        self.arg = arg
 
 @ContractOnly
 @Native
-def test(i: int, i2: int) -> int:
+def test(i: int, i2: int, c: mycoolclass) -> int:
     a = i
     return a + i2
