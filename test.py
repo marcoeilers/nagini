ddef5d34792723bd4c63d5c09ea7c08bbc2091fb
etiennebirling <etienne.birling@gmail.com>
2025-02-11 16:44:05 +0100

    completed test.py for interesting features

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 from nagini_contracts.contracts import *
-from typing import  List
-class mycoolclass:
-    def __init__(self, arg:int )->None:
-        self.arg = arg
+from typing import List
 
-@ContractOnly
-@Native
+
+class mycoolclass:
+    def __init__(self, arg: int) -> None:
+        self.arg = arg
+@Predicate
+def pred(x: mycoolclass, i: int) -> bool:
+    return True
+
 def test(i: int, i2: int, c: mycoolclass) -> int:
-    Requires(i > 0)
-    Requires(i2 > 0 and c.arg > 0)
-    Ensures(Result() > 0)
+    Requires(i > 13 and i > i2+3 and pred(c, i))
+    Requires(i2 > 0 and ((Acc(c.arg) and c.arg > 0) if i2 > 0 else True))
+    Ensures(Result() > 13)
     a = i
     return a + i2
