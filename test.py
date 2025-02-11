80364d04e085765a5f2c23ab2c8e528117341bc7
etiennebirling <etienne.birling@gmail.com>
2025-02-11 18:32:14 +0100

    Add ContractOnly and Native decorators to test function for enhanced validation

--- test.py
+++ test.py
@@ -8,7 +8,8 @@ class mycoolclass:
 @Predicate
 def pred(x: mycoolclass, i: int) -> bool:
     return True
-
+@ContractOnly
+@Native
 def test(i: int, i2: int, c: mycoolclass) -> int:
     Requires(i > 13 and i > i2+3 and pred(c, i))
     Requires(i2 > 0 and ((Acc(c.arg) and c.arg > 0) if i2 > 0 else True))
