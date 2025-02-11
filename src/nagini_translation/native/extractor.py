e53e4ffe89b7cf6fc1f7846058006739ba5458ea
etiennebirling <etienne.birling@gmail.com>
2025-02-11 16:42:18 +0100

    Add EtienneTranslator class and enhance py2vf_context for improved translation handling

--- src/nagini_translation/native/extractor.py
+++ src/nagini_translation/native/extractor.py
@@ -10,17 +10,15 @@ from nagini_translation.lib.program_nodes import (
 )
 from nagini_translation.lib.resolver import get_target as do_get_target
 from nagini_translation.lib.resolver import get_type as do_get_type
+from nagini_translation.translators.common import CommonTranslator
 from typing import Optional
 
 
 class py2vf_context:
-    def __init__(self):
-        self.context = dict()
-        self.parent = None
-
     def __init__(self, p: "py2vf_context" = None):
         self.context = dict()
         self.parent = p
+        self.setup = []
 
     def __getitem__(self, key: str):
         if key in self.context:
@@ -28,12 +26,49 @@ class py2vf_context:
         elif self.parent:
             return self.parent[key]
         else:
-            raise KeyError(key)
+            return None
 
     def __setitem__(self, key: str, value: vf.VFVal):
         self.context[key] = value
 
-
+class EtienneTranslator(CommonTranslator):
+    def __init__(self, f: PythonMethod, ctx: Context, py2vf_ctx: py2vf_context):
+        super().__init__(f, ctx)
+        self.py2vf_ctx = py2vf_ctx
+    def translate_generic(self, node: ast.AST, ctx: Context) -> vf.Fact:
+        if isinstance(node, ast.Compare):
+            return self.translateCompare(node, ctx)
+        if isinstance(node, ast.BoolOp):
+            return self.translateBoolOp(node, ctx)
+        if isinstance(node, ast.Expr):
+            return self.translateExpr(node, ctx)
+        if isinstance(node, ast.Call):
+            pass
+            #return self.translateCall(node, ctx)
+        if isinstance(node, ast.Name):
+            return self.translateName(node, ctx)
+        if isinstance(node, ast.BinOp):
+            return self.translateBinOp(node, ctx)
+        return vf.Fact()
+    def translateCompare(self, node: ast.Compare, ctx: Context) -> vf.Fact:
+        pass
+    def translateBoolOp(self, node: ast.BoolOp, ctx: Context) -> vf.Fact:
+        pass
+    def translateExpr(self, node: ast.Expr, ctx: Context) -> vf.expr:
+        #???
+        pass
+    def translateExprCall(self, node: ast.Call, ctx: Context) -> vf.expr:
+        #becomes a fixpoint call
+        pass
+    def translateFactCall(self, node: ast.Call, ctx: Context) -> vf.Fact:
+        #becomes a predicate
+        pass
+    def translateName(self, node: ast.Name, ctx: Context) -> vf.expr:
+        #becomes a variable, use py2vf_ctx to find the occurence
+        ##therefore case-distinguish about the case in which the variable is used: ref or value?
+        pass
+    def translateBinOp(self, node: ast.BinOp, ctx: Context) -> vf.Fact:
+        pass
 class NativeSpecExtractor:
     def pytype__to__PyObj_t(self, p: PythonType) -> vfpy.PyObj_t:
         return {
@@ -59,21 +94,32 @@ class NativeSpecExtractor:
             self.py2vf_ctx[key + "_ptr"] = vf.VFVal(vf.Pattern(key+"_ptr"))
             tuple_args.append(
                 vf.Pair(self.py2vf_ctx[key+"_ptr"].definition, self.pytype__to__PyObj_t(value.type)))
-        facts = [vfpy.pyobj_hasval(
-            self.py2vf_ctx["args"], vfpy.PyTuple(tuple_args))]
+        self.py2vf_ctx.setup.append(vfpy.pyobj_hasval(
+            self.py2vf_ctx["args"], vfpy.PyTuple(tuple_args)))
         for key, value in f.args.items():
             p, v = self.pyvar__to__PyObj_v(value)
             if (v is not None):
                 self.py2vf_ctx[key+"_val"] = v
-            facts.append(vfpy.pyobj_hasval(self.py2vf_ctx[key+"_ptr"], p))
-        print(vf.FactConjunction(facts))
+            self.py2vf_ctx.setup.append(vfpy.pyobj_hasval(
+                self.py2vf_ctx[key+"_ptr"], p))
+        print(vf.FactConjunction(self.py2vf_ctx.setup))
+        print(self.py2vf_ctx)
         return
+    
+    def precond(self, f: PythonMethod, ctx: Context) -> list[vf.Fact]:
+        #only keep the preconditions: calls to Require
+        Preconds=list(filter(lambda s: isinstance(s.value, ast.Call) and s.value.func.id == "Requires", f.node.body))
+        for precond in Preconds:
+            print(precond.value.args[0])
+        #print(f.node.body.value.func.id)
+        return []
 
     def __init__(self, f: PythonMethod, ctx: Context):
         self.py2vf_ctx = py2vf_context()
         # self.get_type(f.node.body[0].targets[0], ctx)
         # self.get_target(f.node.body[0].targets[0], ctx)
         self.setup(f, ctx)
+        self.precond(f, ctx)
         pass
 
     def extract(self) -> None:
