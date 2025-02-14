be3605bc4eb81c5116a63120e57fd6edf655f9c8
etiennebirling <etienne.birling@gmail.com>
2025-02-14 19:13:37 +0100

    Add generics to Expr class for type safety and clarity

--- src/nagini_translation/native/vf/standard/expr.py
+++ src/nagini_translation/native/vf/standard/expr.py
@@ -1,5 +1,9 @@
 from abc import ABC
+from nagini_translation.native.vf.standard.value import ValueT
 
-class Expr(ABC):
+from typing import Generic
+
+#any expression must return a value in the end...
+class Expr(ABC, Generic[ValueT]):
     def getType(self):
         pass
