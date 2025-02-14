3d5bb66fd0d8e92b5359d3f1e76b0d80801a423d
etiennebirling <etienne.birling@gmail.com>
2025-02-15 00:21:47 +0100

    refined expr class definition

--- src/nagini_translation/native/vf/standard/expr.py
+++ src/nagini_translation/native/vf/standard/expr.py
@@ -1,9 +1,7 @@
 from abc import ABC
 from nagini_translation.native.vf.standard.value import ValueT
-
 from typing import Generic
 
-#any expression must return a value in the end...
 class Expr(ABC, Generic[ValueT]):
-    def getType(self):
-        pass
+    # any expression must return a value in the end...
+    pass
