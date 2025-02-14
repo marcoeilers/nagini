bb85532e2878d2dbf1adf03661a7797add2d8beb
etiennebirling <etienne.birling@gmail.com>
2025-02-14 02:36:09 +0100

    Refactor ValueLocation and ValueEntity classes to improve type handling and encapsulation

--- src/nagini_translation/native/vf/standard/value.py
+++ src/nagini_translation/native/vf/standard/value.py
@@ -1,23 +1,35 @@
 from nagini_translation.native.vf.standard.expr import Expr
+from nagini_translation.native.vf.standard.literal import Literal
+from typing import Type
+
 from abc import ABC
 
 
 class ValueLocation:
-    def __init__(self, type):
+    def __init__(self, type: Type[Literal]):
         self.__content = None
-        self.type = type
+        self.__type = type
 
-    def setContent(self, content: "ValueOccurence"):
+    def setContent(self, content: Expr):
+        # TODO check that the content matches location type
         self.__content = content
 
     def getContent(self):
         return self.__content
 
-
+#DEFINITION: a value is an annotation-internal variable
 class ValueOccurence(Expr, ABC):
-    def __init__(self, location: ValueLocation=None, entity: "ValueEntity"=None):
-        self.location = entity
-        self.entity = location
+    def __init__(self, location: ValueLocation = None, entity: "ValueEntity" = None):
+        self.__location = entity
+        self.__entity = location
+
+
+class Wildcard(ValueOccurence):
+    def __init__(self):
+        pass
+
+    def __str__(self) -> str:
+        pass
 
 
 class ValueDefinition(ValueOccurence):
@@ -37,20 +49,33 @@ class ValueUse(ValueOccurence):
 
 
 class ValueEntity:
-    def __init__(self):
+    def __init__(self, type: Type[Literal], name: str):
         self.__occurences = []
-        self.__definition = None
+        self.__def = None
+        self.__type = type
+        self.__name = str
 
+    def getType(self):
+        return self.__type
+    
+    def getName(self):
+        return self.__name
+    
     def addLocation(self, loc: ValueLocation):
+        # TODO check that the location matches entity type
         self.__occurences.append(loc)
-        loc.content = self
+        loc.setContent(self)
 
     def removeLocation(self, loc: ValueLocation):
         self.__occurences.remove(loc)
         loc.content = None
 
-    def setDefinition(self, defn: ValueDefinition):
-        self.__definition = defn
+    def setDef(self, defn: ValueDefinition):
+        # TODO check that the location matches entity type
+        self.__def = defn
+
+    def getDef(self):
+        return self.__def
 
     def getLocations(self):
         return self.__occurences[:]
