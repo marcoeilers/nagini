8d5b9325b015e1bad4680b35db32917303b0d441
etiennebirling <etienne.birling@gmail.com>
2025-02-15 01:19:38 +0100

    managed to find a good way to define predicates

--- src/nagini_translation/native/vf/standard/pred.py
+++ src/nagini_translation/native/vf/standard/pred.py
@@ -1,2 +1,10 @@
-class Pred():
-    pass
+from typing import Type, Tuple, TypeVar, Generic
+from nagini_translation.native.vf.standard.value import Value
+
+_T = TypeVar("T", bound=Tuple[Value, ...])
+
+
+class Pred(Generic[_T]):
+    def __init__(self, pred: Type[_T], args: _T):
+        self.pred = pred
+        self.args = args
