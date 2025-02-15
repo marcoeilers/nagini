7adb94a8cfcc1fd2eb9ebe0667869de34bd7b4b9
etiennebirling <etienne.birling@gmail.com>
2025-02-15 23:32:03 +0100

    add ImmediateLiteral class to represent immediate values in expressions

--- src/nagini_translation/native/vf/standard/expr.py
+++ src/nagini_translation/native/vf/standard/expr.py
@@ -7,3 +7,10 @@ ValueT = TypeVar("ValueT", bound="Value")
 class Expr(ABC, Generic[ValueT]):
     # any expression must return a value in the end...
     pass
+
+class ImmediateLiteral(Expr[ValueT]):
+    def __init__(self, value: ValueT):
+        self.__value = value
+
+    #def getValue(self) -> ValueT:
+    #    return self.__value
