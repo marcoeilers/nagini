7084d1a02c89b33c1f4558e17311ed40047c6daf
etiennebirling <etienne.birling@gmail.com>
2025-02-15 23:43:57 +0100

    a first version of pymodules

--- src/nagini_translation/native/vf/pymodules.py
+++ src/nagini_translation/native/vf/pymodules.py
@@ -1,39 +1,25 @@
-from nagini_translation.native.vf.standard.pred import Pred
+from nagini_translation.native.vf.standard.fact import PredicateFact
 from nagini_translation.native.vf.standard.value import Value
 from nagini_translation.native.vf.standard.literal import Ptr, Char
 from nagini_translation.native.vf.standard.inductive import List, Pair, Inductive
 from nagini_translation.native.vf.standard.expr import Expr
+from nagini_translation.native.vf.standard.valueloc import ValueLocation
 from typing import TypeVar, Tuple, Type
 from abc import ABC
 
 ValueT = TypeVar("ValueT", bound="Value")
+
+
 class PyObj_v(Inductive, ABC):
     pass
+
+
 class PyLong(PyObj_v):
     def __init__(self, value: int):
         self.value = value
 
     def __str__(self):
         return "PyLong_v("+str(self.value)+")"
-class PyObj_t(Inductive, Type[PyObj_v]):
-    #TODO: review this declaration: is really what we want?
-    pass
-class PyObjPtr(Ptr):
-    pass
-PyObjPtrT = TypeVar('PyObjPtrT', bound=PyObjPtr)
-
-PyObj_HasVal = Pred[Tuple[PyObjPtrT, ValueT]]("pyobj_hasval", Tuple[PyObjPtr, Value])
-PyObj_HasAttr = Pred[Tuple[PyObjPtrT, List[Char], PyObjPtrT]]("pyobj_hasattr", Tuple[PyObjPtr, List[Char], PyObjPtr])
-
-
-
-class PyTuple(PyObj_v):
-    # TODO: a pointer is represented as a an expression here, but could it be refined as a val? decude whe we'll define the class ptr
-    def __init__(self, items: List[Pair[Expr, PyObj_t]]):
-        self.items = items
-
-    def __str__(self):
-        return "PyTuple_v("+(",\n\t".join(map(str, self.items)))+")"
 
 
 class PyClass(Inductive, ABC):
@@ -45,6 +31,19 @@ class PyClass(Inductive, ABC):
         return "PyClass(\""+self.name+"\", "+(str(self.parent) if self.parent != None else "ObjectType")+")"
 
 
+class PyClassInstance(PyObj_v):
+    def __init__(self, type: PyClass):
+        self.type = type
+
+    def __str__(self):
+        return "PyClassInstance_v("+str(self.type)+")"
+
+
+class PyObj_t(Inductive, ABC):
+    # TODO: review this declaration: is really what we want?
+    pass
+
+
 class PyClass_t(PyObj_t):
     def __init__(self, type: PyClass):
         self.type = type
@@ -53,9 +52,39 @@ class PyClass_t(PyObj_t):
         return "PyClass_t("+str(self.type)+")"
 
 
-class PyClassInstance(PyObj_v):
-    def __init__(self, type: PyClass):
-        self.type = type
+class PyObjPtr(Ptr):
+    pass
+
+
+class PyObj_HasValue(PredicateFact):
+    def __init__(self, ptr: Expr[PyObjPtr], value: Expr[PyObj_v]):
+        self.ptrLoc = ValueLocation[PyObjPtr]()
+        self.ptrLoc.setContent(ptr)
+        self.valueLoc = ValueLocation[PyObj_v]()
+        self.valueLoc.setContent(value)
 
     def __str__(self):
-        return "PyClassInstance_v("+str(self.type)+")"
+        return "pyobj_hasvalue("+str(self.ptrLoc)+", "+str(self.valueLoc)+")"
+
+
+class PyObj_HasAttr(PredicateFact):
+    def __init__(self, obj: Expr[PyObjPtr], attrName: Expr[Char], attrValue: Expr[PyObj_v]):
+        self.objLoc = ValueLocation[PyObjPtr]()
+        self.objLoc.setContent(obj)
+        self.attrNameLoc = ValueLocation[Char]()
+        self.attrNameLoc.setContent(attrName)
+        self.attrValueLoc = ValueLocation[PyObj_v]()
+        self.attrValueLoc.setContent(attrValue)
+
+    def __str__(self):
+        return "pyobj_hasattr("+str(self.objLoc)+", "+str(self.attrNameLoc)+", "+str(self.attrValueLoc)+")"
+
+
+class PyTuple(PyObj_v):
+    # TODO: a pointer is represented as a an expression here, but could it be refined as a val? decude whe we'll define the class ptr
+    def __init__(self, items: List[Pair[PyObjPtr, PyObj_t]]):
+        pass
+        # self.items =
+
+    def __str__(self):
+        return "PyTuple_v("+(",\n\t".join(map(str, self.items)))+")"
