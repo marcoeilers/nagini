468df6df49570b6a88b9e6bddf3ab2dd60326ee7
etiennebirling <etienne.birling@gmail.com>
2025-02-15 23:52:38 +0100

    abstracted simplified PredicateFact abstract declaration

--- src/nagini_translation/native/vf/standard/fact.py
+++ src/nagini_translation/native/vf/standard/fact.py
@@ -1,6 +1,5 @@
 from abc import ABC
 from typing import Tuple, TypeVar, Generic
-from nagini_translation.native.vf.standard.pred import Pred
 from nagini_translation.native.vf.standard.expr import Expr
 from nagini_translation.native.vf.standard.value import Value
 
@@ -16,13 +15,8 @@ class BooleanFact(Fact):  # a fact built using any boolean ast.expression
 _T = TypeVar("T", bound=Tuple[Value, ...])
 
 
-class PredicateFact(Fact, ABC, Generic[_T]):  # a fact built using a predicate
-    def __init__(self, pred: Pred[_T]):
-        self.pred = pred
-        # now instantiate valuelocation with suitable type arguments to contain the predicate argument
-
-    def __str__(self) -> str:
-        return self.pred.name + "(" + ", ".join(map(str, self.args)) + ")"
+class PredicateFact(Fact, ABC):  # a fact built using a predicate
+    pass
 
 
 class FactConjunction(Fact):
--- src/nagini_translation/native/vf/test.py
+++ src/nagini_translation/native/vf/test.py
@@ -1,5 +1,7 @@
-import nagini_translation.native.vf.pymodules as pymodules
-import nagini_translation.native.vf.standard as standard
+import nagini_translation.native.vf.pymodules as vfpy
+import nagini_translation.native.vf.standard.literal as lit
+import nagini_translation.native.vf.standard.expr as expr
+import nagini_translation.native.vf.standard as vf
 
 def main():
-    pass
+    pred = vfpy.PyObj_HasVal(lit.Ptr(), expr.ImmediateLiteral(lit.Int()));
