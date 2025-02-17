83f43b125139b3f4dae88a9717ea8d945909f6fc
etiennebirling <etienne.birling@gmail.com>
2025-02-17 16:48:03 +0100

    just some code cleaning result from precedent updates

--- src/nagini_translation/native/vf/standard/inductive.py
+++ src/nagini_translation/native/vf/standard/inductive.py
@@ -1,9 +1,6 @@
 from abc import ABC
 from typing import Generic, TypeVar
 from nagini_translation.native.vf.standard.value import Value
-from nagini_translation.native.vf.standard.valueloc import ValueLocation
-ValueT = TypeVar("ValueT", bound="Value")
-ValueT2 = TypeVar("ValueT2", bound="Value")
 
 
 class Inductive(Value, ABC):
--- src/nagini_translation/native/vf/vf.py
+++ src/nagini_translation/native/vf/vf.py
@@ -2,27 +2,28 @@ from nagini_translation.native.vf.standard.expr import *
 from nagini_translation.native.vf.standard.fact import *
 from nagini_translation.native.vf.standard.inductive import *
 from nagini_translation.native.vf.standard.literal import *
-from nagini_translation.native.vf.standard.name import *
 from nagini_translation.native.vf.standard.value import *
-from nagini_translation.native.vf.standard.valueloc import *
 
 _ValueT2 = TypeVar("_ValueT2", bound="Value")
 _ValueT = TypeVar("_ValueT", bound="Value")
 
 
-class Pair(Inductive, Generic[ValueT, ValueT2]):
-    def __init__(self, first: ValueT, second: ValueT2):
-        self.__first = ValueLocation[ValueT](first)
-        self.__second = ValueLocation[ValueT](second)
+class Pair(Inductive, Generic[_ValueT, _ValueT2]):
+    def __init__(self, first: _ValueT, second: _ValueT2):
+        self.__first = ValueLocation[_ValueT](first)
+        self.__second = ValueLocation[_ValueT](second)
 
-class List(Inductive, Generic[ValueT], ABC):
+class List(Inductive, Generic[_ValueT], ABC):
+    @staticmethod
+    def from_list(lst: list[_ValueT]) -> "List"[_ValueT]:
+        if len(lst) == 0:
+            return Nil()
+        else:
+            return Cons(lst[0], List.from_list(lst[1:]))
 
-    def __init__(self, head: ValueT, tail: list[ValueT]):
-        pass
 
-
-class Cons(List[ValueT]):
-    def __init__(self, head: ValueT, tail: list[ValueT]):
+class Cons(List[_ValueT]):
+    def __init__(self, head: _ValueT, tail: list[_ValueT]):
         self.__head = head
         self.__tail = tail
 
