3d1793783c3d6d2676a0d358b87995c0b3a2a08e
etiennebirling <etienne.birling@gmail.com>
2025-02-17 17:11:13 +0100

    finished setting up things to cleanly represent inductive expressions (with possibility for definitions inside)

--- src/nagini_translation/native/vf/standard/expr.py
+++ src/nagini_translation/native/vf/standard/expr.py
@@ -1,16 +1,43 @@
 from abc import ABC
-from nagini_translation.native.vf.standard.value import Value
-from typing import Generic
-from typing import TypeVar
+from nagini_translation.native.vf.standard.value import Value, ValueLocation, NamedValue
+from typing import Generic, TypeVar
 
-ValueT = TypeVar("ValueT", bound="Value")
-class Expr(ABC, Generic[ValueT]):
+_ValueT = TypeVar("ValueT", bound="Value")
+class Expr(ABC, Generic[_ValueT]):
     # any expression must return a value in the end...
     pass
 
-class ImmediateLiteral(Expr[ValueT]):
-    def __init__(self, value: ValueT):
-        self.__value = value
+class NameOccurence(Generic[_ValueT], ABC):
+    def __init__(self, location: ValueLocation = None, entity: "NamedValue[_ValueT]" = None):
+        self.__location = location
+        self.__entity = entity
+        entity.addLocation(location)
 
-    #def getValue(self) -> ValueT:
-    #    return self.__value
+
+class NameDefExpr(NameOccurence[_ValueT]):
+    def __init__(self):
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+
+class DefLessExpr(Expr[_ValueT], ABC):
+    #definitionless expression: only uses names, no definitions
+    def __init__(self):
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+
+class NameUseExpr(NameOccurence[_ValueT], DefLessExpr[_ValueT]):
+    def __init__(self):
+        pass
+
+    def __str__(self) -> str:
+        pass
+
+class ImmediateLiteral(DefLessExpr[_ValueT]):
+    def __init__(self, value: _ValueT):
+        self.__value = value
--- src/nagini_translation/native/vf/standard/inductive.py
+++ src/nagini_translation/native/vf/standard/inductive.py
@@ -1,9 +1,10 @@
 from abc import ABC
 from typing import Generic, TypeVar
 from nagini_translation.native.vf.standard.value import Value
+from nagini_translation.native.vf.standard.expr import Expr
+
+ExprT = TypeVar("ExprT", bound="Expr")
 
 
-class Inductive(Value, ABC):
+class Inductive(Value, ABC, Generic[ExprT]):
     pass
-
-
