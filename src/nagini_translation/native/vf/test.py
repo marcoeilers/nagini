0bacc154ec48e4d083c9e005dd2f64904d86f23a
etiennebirling <etienne.birling@gmail.com>
2025-02-18 14:50:44 +0100

    removed obsolete test.py file for VF files

--- src/nagini_translation/native/vf/test.py
+++
@@ -1,7 +0,0 @@
-import nagini_translation.native.vf.pymodules as vfpy
-import nagini_translation.native.vf.standard.literal as lit
-import nagini_translation.native.vf.standard.expr as expr
-import nagini_translation.native.vf.standard as vf
-
-def main():
-    pred = vfpy.PyObj_HasVal(lit.Ptr(), expr.ImmediateLiteral(lit.Int()));
