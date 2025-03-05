d7f2050b541cd3bf84df2788e88fabdd5ffc9aa2
etiennebirling <etienne.birling@gmail.com>
2025-03-05 16:25:11 +0100

    improve test runner to handle empty solutions and streamline output

--- src/nagini_translation/native/runtests.py
+++ src/nagini_translation/native/runtests.py
@@ -10,17 +10,20 @@ for filename in os.listdir('nagini_translation/native/tests'):
     if filename.startswith("test_"):
         print("Running test for file: " + filename + (30 - len(filename)) * " ", end="")
         filepath = 'nagini_translation/native/tests/' + filename
-        result = subprocess.run('python3 nagini_translation/main.py --skip-verification ' +
-                                filepath, cwd="./", shell=True, capture_output=True, text=True)
         with open(filepath, "r", encoding="utf-8") as file:
             tree = ast.parse(file.read(), filename=filepath)
         functions = [node for node in ast.walk(tree) if isinstance(node, ast.FunctionDef)]
         function_sols = ["" if ast.get_docstring(f)==None else ast.get_docstring(f) for f in functions]
         solution = "\n".join(function_sols)
-        print(""+((Fore.GREEN+"PASSED") if result.stdout == solution else (Fore.RED+"FAILED"))+Style.RESET_ALL)
-        #print(hline+result.stdout+"\n"+hline+solution+hline)
-        if(result.stdout != solution):
-            differences=difflib.ndiff(result.stdout.splitlines(), solution.splitlines())
-            print("\n".join(differences))
+        if solution=="":
+            print(Fore.BLUE+"EMPTY"+Style.RESET_ALL)
+        else:
+            result = subprocess.run('python3 nagini_translation/main.py --skip-verification ' +
+                                    filepath, cwd="./", shell=True, capture_output=True, text=True)
+            print(""+((Fore.GREEN+"PASSED") if result.stdout == solution else (Fore.RED+"FAILED"))+Style.RESET_ALL)
+            #print(hline+result.stdout+"\n"+hline+solution+hline)
+            if(result.stdout != solution):
+                differences=difflib.ndiff(result.stdout.splitlines(), solution.splitlines())
+                print("\n".join(differences))
 file.close()
 print("All tests completed.")
