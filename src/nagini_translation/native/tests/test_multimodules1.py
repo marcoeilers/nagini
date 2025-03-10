ed7f0faed6404250bbb0b7e91936d1fe4bad92b5
etiennebirling <etienne.birling@gmail.com>
2025-03-10 23:55:41 +0100

    Add new test file test_multimodules1.py with initial implementation of compare3 function

---
+++ src/nagini_translation/native/tests/test_multimodules1.py
@@ -0,0 +1,24 @@
+"""
+fixpoint PyClass PyClass_ObjectType(){
+      return ObjectType;
+}
+fixpoint PyClass PyClass_module_1mytupledclass(){
+      return PyClass("module_1mytupledclass", PyClass_ObjectType);
+}
+"""
+import nagini_translation.t1 as t1
+from nagini_contracts.contracts import *
+
+@ContractOnly
+@Native
+#TODO: this passes the translation and fails the verification
+def compare3(c: t1.mytupledclass) -> int:
+    """
+    pyobj_hasvalue(args, PyTuple_v(cons(pair(?c__ptr, PyClass_t(PyClass_module_1mytupledclass)), nil))) &*&
+    pyobj_hasvalue(c__ptr, PyClassInstance_v(PyClass_module_1mytupledclass)) &*&
+    pyobj_hasattr(c__ptr, arg, ?c_DOT_arg__ptr) &*&
+    pyobj_hasvalue(c_DOT_arg__ptr, PyLong_v(?c_DOT_arg__val)) &*&
+    false
+    """
+    Requires(Acc(c.arg) and c.arg == (3,(21,12)))
+    Ensures(Result() > 13)
