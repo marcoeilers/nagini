17c6f9777f1d211f3a39caed2b761be4f0dd68dc
etiennebirling <etienne.birling@gmail.com>
2025-03-10 15:44:48 +0100

    Refactor output handling in runtests to improve separation of environment and function results

--- src/nagini_translation/native/runtests.py
+++ src/nagini_translation/native/runtests.py
@@ -11,17 +11,19 @@ def handle_diff(sol, res):
     if (sol == None):
         print(Fore.RED+"EMPTY"+Style.RESET_ALL)
     else:
-        print(""+((Fore.GREEN+"PASSED") if res ==
-            sol else (Fore.RED+"FAILED"))+Style.RESET_ALL)
-        if (res != sol):
+        sol1 = sol.replace("\r", "").replace("\n", "").replace(" ", "").replace("\t", "")
+        res1 = res.replace("\r", "").replace("\n", "").replace(" ", "").replace("\t", "")
+        if (res1 == sol1):
+            print(""+(Fore.GREEN+"PASSED")+Style.RESET_ALL)
+        else:
+            print((Fore.RED+"FAILED")+Style.RESET_ALL)
             differences = list(difflib.ndiff(res.splitlines(), sol.splitlines()))
-            #return "\n\t" + "\n\t".join(differences)
             print("\n\t" + "\n\t".join(filter(lambda x: x.startswith("+") or x.startswith("-") or x.startswith("?"), differences)))
 
 
 for filename in os.listdir('nagini_translation/native/tests'):
     if filename.startswith("test_"):
-        print("Running test for file: " + filename, end="")
+        print("Running test for file: " + filename)
         filepath = 'nagini_translation/native/tests/' + filename
         with open(filepath, "r", encoding="utf-8") as file:
             tree = ast.parse(file.read(), filename=filepath)
@@ -33,13 +35,13 @@ for filename in os.listdir('nagini_translation/native/tests'):
         )]
         result = subprocess.run('python3 nagini_translation/main.py --skip-verification ' +
                                 filepath, cwd="./", shell=True, capture_output=True, text=True)
-        print("\n  ENV" + (20 - len("ENV")) * " ", end="")
-        handle_diff(ast.get_docstring(tree), result.stdout.split("/*--END OF ENV--*/")[0])
-        if (len(result.stdout.split("/*--END OF ENV--*/")) > 1):
-            fun = result.stdout.split("/*--END OF ENV--*/")[1] or None
+        print("  ENV" + (20 - len("ENV")) * " ", end="")
+        separated=result.stdout.split("/*--END OF ENV--*/\n")
+        handle_diff(ast.get_docstring(tree), separated[0])
+        if (len(separated) > 1):
+            fun = separated[1] or None
             for i, f in enumerate(functions):
-                print("\n  "+f.name + (20 - len(f.name)) * " ", end="")
-                handle_diff(ast.get_docstring(f), fun.split("/*----*/")[i])
-        print()
+                print("  "+f.name + (20 - len(f.name)) * " ", end="")
+                handle_diff(ast.get_docstring(f), fun.split("\n/*----*/\n")[i])
 file.close()
 print("All tests completed.")
