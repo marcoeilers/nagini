f00368292c096b4d270a6f08fd18e19d9042529f
etiennebirling <etienne.birling@gmail.com>
2025-04-19 14:27:16 +0200

    cleaned bincoeff for partial correctness verification

--- src/nagini_translation/native/case_studies/bincoeff.c
+++ src/nagini_translation/native/case_studies/bincoeff.c
@@ -1,44 +1,25 @@
-/// inspired by GMPy_MPZ_Function_Bincoef 
+/// inspired by GMPy_MPZ_Function_Bincoef
 #include <Python.h>
 #include <gmp.h>
-#include "safe_mpz_alloc.c"
 static PyObject *
-GMPy_MPZ_Function_Bincoef(PyObject *self, PyObject * const *args)
+GMPy_MPZ_Function_Bincoef(PyObject *self, PyObject *const *args)
 {
-    MPZ_Object *result = NULL, *tempx;
     unsigned long n, k;
-
-    /*if (nargs != 2) {
-        TYPE_ERROR("bincoef() requires two integer arguments");
-        return NULL;
-    }*/
-   //this block can be safely ignored as it is now safe to assume that args tuple has length 2
-   
-    mpz_t x;            // Declare the variable (not a pointer!)
-    if(mpz_safe_init(x)){
-        return NULL;
-    }
+    mpz_t x;     // Declare the variable (not a pointer!)
+    mpz_init(x); // Initialize the variable
     k = PyLong_AsUnsignedLong(PyTuple_GET_ITEM(args, 1));
-    if (k == (unsigned long)(-1) && PyErr_Occurred()) {
+    if (k == (unsigned long)(-1) && PyErr_Occurred())
+    {
         return NULL;
     }
 
-    n = PyLong_AsUnsignedLong(args[0]);
-    if (n == (unsigned long)(-1) && PyErr_Occurred()) {
-        PyErr_Clear();
-    }
-    else {
-        /* Use mpz_bin_uiui which should be faster. */
-        mpz_bin_uiui(result->z, n, k);
-        return (PyObject*)result;
-    }
-
-    if (!(tempx = GMPy_MPZ_From_Integer(args[0], NULL))) {
-        Py_DECREF((PyObject*)result);
+    n = PyLong_AsUnsignedLong(PyTuple_GET_ITEM(args, 0));
+    if (n == (unsigned long)(-1) && PyErr_Occurred())
+    {
         return NULL;
     }
-    mpz_get_ui();
-    mpz_bin_ui(result->z, tempx->z, k);
-    Py_DECREF((PyObject*)tempx);
-    return (PyObject*)result;
+    mpz_bin_uiui(x, n, k);
+    long res = mpz_get_ui(x);
+    mpz_clear(x); // Clear the variable
+    return PyLong_FromUnsignedLong(res);
 }
