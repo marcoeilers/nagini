ddd53a428b2ac7f6aaf4d5fa272a63801e402625
etiennebirling <etienne.birling@gmail.com>
2025-04-22 14:14:37 +0200

    refactor: update parameter names in bincoeff and fibo functions for clarity

--- src/nagini_translation/native/case_studies/fibo_array.py
+++ src/nagini_translation/native/case_studies/fibo_array.py
@@ -1,12 +1,12 @@
 from nagini_contracts.contracts import *
 from typing import List
 @Pure
-def fibo(n:int) -> int:
-    return 1 if n <= 0 else fibo(n-1) + fibo(n-2)
+def fibo(VALUEONLY_n:int) -> int:
+    return 1 if VALUEONLY_n <= 0 else fibo(VALUEONLY_n-1) + fibo(VALUEONLY_n-2)
 
 @ContractOnly
 @Native
 def fibo_array(n: int, l: List[int]) -> None:
     Requires(n >= 0)
-    Ensures(list_pred(Result()) and
+    Ensures(list_pred(l) and
             Forall(int, lambda i: Implies(i >= 0 and i < len(l), l[i] == fibo(i))))
