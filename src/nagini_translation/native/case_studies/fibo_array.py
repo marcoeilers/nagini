aa5cf2359b0cb148956d97673b98c5a5c87d62fa
etiennebirling <etienne.birling@gmail.com>
2025-04-22 21:00:44 +0200

    refactor: improve base case handling in fibo function for clarity

--- src/nagini_translation/native/case_studies/fibo_array.py
+++ src/nagini_translation/native/case_studies/fibo_array.py
@@ -2,7 +2,10 @@ from nagini_contracts.contracts import *
 from typing import List
 @Pure
 def fibo(VALUEONLY_n:int) -> int:
-    return 1 if VALUEONLY_n <= 0 else fibo(VALUEONLY_n-1) + fibo(VALUEONLY_n-2)
+    return 1 if VALUEONLY_n == 0 else\
+           1 if VALUEONLY_n == 1 else\
+           1 if VALUEONLY_n <0 else\
+           fibo(VALUEONLY_n-1) + fibo(VALUEONLY_n-2)
 
 @ContractOnly
 @Native
