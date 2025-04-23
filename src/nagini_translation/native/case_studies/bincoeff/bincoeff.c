3b455869dc4a28309319896a59c26fe6c7c086ae
etiennebirling <etienne.birling@gmail.com>
2025-04-23 04:05:41 +0200

    refactor: reorganize mpz_include.c and update bincoef function for improved structure and clarity

--- src/nagini_translation/native/case_studies/bincoeff/bincoeff.c
+++ src/nagini_translation/native/case_studies/bincoeff/bincoeff.c
@@ -4,8 +4,13 @@
 #include <gmp.h>
 #else
 #include "mpz_include.c"
-#include "vfpy.c"
+#include "../vfpy/PythonAPI/vfpy.c"
 #endif
+/*@
+    lemma_auto void PURE_bincoeff__2__bin_mpz(int n, int k);
+    requires true;
+    ensures PURE_bincoeff(n, k) == bin_mpz(n, k);
+@*/
 
 /*@
 fixpoint PyClass PyClass_ObjectType(){
@@ -17,7 +22,7 @@ fixpoint int PURE_bincoeff(int VALUEONLY_n__val, int VALUEONLY_k__val){
 @*/
 /*--END OF ENV--*/
 static PyObject *
-GMPy_MPZ_Function_Bincoef(PyObject *self, PyObject *const *args)
+GMPy_MPZ_Function_Bincoef(PyObject *self, PyObject  *args)
 /*@
 requires PyExc(none, none) &*&
 pyobj_hasval(args, PyTuple_v(cons(pair(?n__ptr, PyLong_t), cons(pair(?k__ptr, PyLong_t), nil)))) &*&
@@ -31,28 +36,34 @@ pyobj_hasval(args, PyTuple_v(cons(pair(n__ptr, PyLong_t), cons(pair(k__ptr, PyLo
 pyobj_hasval(n__ptr, PyLong_v(n__val)) &*&
 pyobj_hasval(k__ptr, PyLong_v(k__val)) &*&
 pyobj_hasval(result, PyLong_v(?result__val)) &*&
-(result__val == PURE_bincoeff(n__val, k__val));
+result__val == PURE_bincoeff(n__val, k__val);
 @*/
 
 {
+    //@ assert n__val <= 63;
+    //@ assert ULONG_MAX > 63;
+    PyObject *obj_n = PyTuple_GetItem(args, 0);
+    
     unsigned long n, k;
-    mpz_t x;     // Declare the variable (not a pointer!)
+    struct __mpz_struct x_s = {0, 0, NULL}; 
+    mpz_t x= &x_s;     // Declare the variable (not a pointer!)
     mpz_init(x); // Initialize the variable
-    k = PyLong_AsUnsignedLong(PyTuple_GET_ITEM(args, 1));
-    if (k == (unsigned long)(-1) && PyErr_Occurred())
-    {
-        return NULL;
-    }
 
-    n = PyLong_AsUnsignedLong(PyTuple_GET_ITEM(args, 0));
-    if (n == (unsigned long)(-1) && PyErr_Occurred())
+    PyGILState_STATE gstate = PyGILState_Ensure();
+    n = PyLong_AsUnsignedLong(PyTuple_GetItem(args, 0));
+    /*if (PyErr_Occurred())
     {
         return NULL;
-    }
+    }*/
+    PyObject *obj_k = PyTuple_GetItem(args, 1);
+    k = PyLong_AsUnsignedLong(obj_k);
+    /*if (PyErr_Occurred())
+    {
+        return NULL;
+    }*/
+    PyGILState_Release(gstate);
     mpz_bin_uiui(x, n, k);
-    long res = mpz_get_ui(x);
-    //todo check for overflow
-    //if overflow, return NULL; and raise exception? no... unsupported...
-    mpz_clear(x); // Clear the variable
+    unsigned long res = mpz_get_ui(x);
+    mpz_clear(x); // Clear the variable*/
     return PyLong_FromUnsignedLong(res);
 }
--- src/nagini_translation/native/case_studies/bincoeff/mpz_include.c
+++ src/nagini_translation/native/case_studies/bincoeff/mpz_include.c
@@ -1,16 +1,15 @@
 //#include <gmp.h>
 
 typedef unsigned long int	mp_limb_t;
-typedef struct
-{
+struct __mpz_struct{
   int _mp_alloc;		/* Number of *limbs* allocated and pointed
 				   to by the _mp_d field.  */
   int _mp_size;			/* abs(_mp_size) is the number of limbs the
 				   last field points to.  If _mp_size is
 				   negative this is a negative number.  */
   mp_limb_t *_mp_d;		/* Pointer to the limbs.  */
-} __mpz_struct;
-typedef __mpz_struct* mpz_t;
+};
+typedef struct __mpz_struct* mpz_t;
 //@predicate is_mpz(mpz_t x; int val);
 
 void mpz_init(mpz_t x);
@@ -18,12 +17,20 @@ void mpz_init(mpz_t x);
 /*@ensures is_mpz(x,_);@*/
 
 /*@
-fixpoint unsigned int bincoeff(unsigned int n, unsigned int k) {
-    return (k == 0) ? 1 :
-           (n == 0) ? 0 :
-           bincoeff(n-1, k-1) + bincoeff(n-1, k);
+fixpoint int bin_mpz(unsigned int n, unsigned int k){
+  return ((k == 0) ? 1 : 
+  ((n == k) ? 1 : 
+  (bin_mpz((n - 1), (k - 1)) + 
+  bin_mpz((n - 1), k))));
 }
 @*/
-void mpz_bin_uiui(mpz_t res, unsigned int n, unsigned int k);
+void mpz_bin_uiui(mpz_t res, unsigned long n, unsigned long k);
 /*@requires is_mpz(res,_) &*& n >= 0 &*& k >= 0 &*& k<=n;@*/
-/*@ensures is_mpz(res, ?res_val) &*& res_val == bincoeff(n, k);@*/
+/*@ensures is_mpz(res, ?res_val) &*& res_val == bin_mpz(n, k);@*/
+
+unsigned long mpz_get_ui(mpz_t x);
+/*@requires is_mpz(x, ?val);@*/
+/*@ensures is_mpz(x, val) &*& result == val;@*/
+void mpz_clear(mpz_t x);
+/*@requires is_mpz(x, _);@*/
+/*@ensures true;@*/
