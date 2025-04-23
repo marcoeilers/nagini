f69467741bca8b7af939a9f1c7f91d4c06e68e3d
etiennebirling <etienne.birling@gmail.com>
2025-04-23 04:15:50 +0200

    refactor: improve GMPy_MPZ_Function_Bincoef for clarity and error handling

--- src/nagini_translation/native/case_studies/bincoeff/bincoeff.c
+++ src/nagini_translation/native/case_studies/bincoeff/bincoeff.c
@@ -22,14 +22,14 @@ fixpoint int PURE_bincoeff(int VALUEONLY_n__val, int VALUEONLY_k__val){
 @*/
 /*--END OF ENV--*/
 static PyObject *
-GMPy_MPZ_Function_Bincoef(PyObject *self, PyObject  *args)
+GMPy_MPZ_Function_Bincoef(PyObject *self, PyObject *args)
 /*@
 requires PyExc(none, none) &*&
 pyobj_hasval(args, PyTuple_v(cons(pair(?n__ptr, PyLong_t), cons(pair(?k__ptr, PyLong_t), nil)))) &*&
 pyobj_hasval(n__ptr, PyLong_v(?n__val)) &*&
 pyobj_hasval(k__ptr, PyLong_v(?k__val)) &*&
 ((((n__val >= 0) && (k__val >= 0)) && (k__val <= n__val)) && (n__val <= 63));
-@*/ 
+@*/
 /*@
 ensures PyExc(none, none) &*&
 pyobj_hasval(args, PyTuple_v(cons(pair(n__ptr, PyLong_t), cons(pair(k__ptr, PyLong_t), nil)))) &*&
@@ -43,11 +43,11 @@ result__val == PURE_bincoeff(n__val, k__val);
     //@ assert n__val <= 63;
     //@ assert ULONG_MAX > 63;
     PyObject *obj_n = PyTuple_GetItem(args, 0);
-    
+
     unsigned long n, k;
-    struct __mpz_struct x_s = {0, 0, NULL}; 
-    mpz_t x= &x_s;     // Declare the variable (not a pointer!)
-    mpz_init(x); // Initialize the variable
+    struct __mpz_struct x_s = {0, 0, NULL};
+    mpz_t x = &x_s; // Declare the variable (not a pointer!)
+    mpz_init(x);    // Initialize the variable
 
     PyGILState_STATE gstate = PyGILState_Ensure();
     n = PyLong_AsUnsignedLong(PyTuple_GetItem(args, 0));
@@ -61,9 +61,23 @@ result__val == PURE_bincoeff(n__val, k__val);
     {
         return NULL;
     }*/
-    PyGILState_Release(gstate);
     mpz_bin_uiui(x, n, k);
     unsigned long res = mpz_get_ui(x);
     mpz_clear(x); // Clear the variable*/
-    return PyLong_FromUnsignedLong(res);
+    //@ assert PyExc(none, none);
+    // 
+    PyObject *r = NULL;
+    r = PyLong_FromUnsignedLong(res);
+    while(!PyErr_Occurred())
+    //@ invariant true;
+    {
+        r = PyLong_FromUnsignedLong(res);
+        if(r != NULL){
+            PyErr_Clear();
+            break;
+        }
+    } 
+    PyGILState_Release(gstate);
+    PyErr_Clear();
+    return r;
 }
