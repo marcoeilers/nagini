553abb0f1eb62f17b339734ced7db091b08f685e
etiennebirling <etienne.birling@gmail.com>
2025-04-23 10:49:55 +0200

    simplified annotations of the bincoeff

--- src/nagini_translation/native/case_studies/bincoeff/bincoeff.c
+++ src/nagini_translation/native/case_studies/bincoeff/bincoeff.c
@@ -40,42 +40,29 @@ pyobj_hasval(result, PyLong_v(?result__val)) &*&
 result__val == PURE_bincoeff(n__val, k__val);
 @*/
 {
-    //@ assert n__val <= 63;
-    //@ assert ULONG_MAX > 63;
     PyObject *obj_n = PyTuple_GetItem(args, 0);
-
     unsigned long n, k;
     struct __mpz_struct x_s = {0, 0, NULL};
     mpz_t x = &x_s; // Declare the variable (not a pointer!)
     mpz_init(x);    // Initialize the variable
-
     n = PyLong_AsUnsignedLong(PyTuple_GetItem(args, 0));
     PyObject *obj_k = PyTuple_GetItem(args, 1);
     k = PyLong_AsUnsignedLong(obj_k);
     mpz_bin_uiui(x, n, k);
     unsigned long res = mpz_get_ui(x);
-    //@assert res == (bin_mpz(n, k));
     mpz_clear(x); 
-    //@assert PyExc(?e_1, ?t_1);
     PyObject *r = PyLong_FromUnsignedLong(res);
-    //@ assert (r == NULL)?PyExc( some(_), some(_)):PyExc(e_1, t_1);
-    //@ assert (r == NULL)?true:pyobj_hasval(r, PyLong_v(res));
     while (r == NULL) 
 /*@
     invariant (r == NULL)?true:pyobj_hasval(r, PyLong_v(res)) &*&
-    (r == NULL)?PyExc( some(_), some(_)):PyExc(e_1, t_1) &*&
+    (r == NULL)?PyExc( some(_), some(_)):PyExc(none, none) &*&
     pyobj_hasval(args, PyTuple_v(cons(pair(n__ptr, PyLong_t), cons(pair(k__ptr, PyLong_t), nil)))) &*&
     pyobj_hasval(n__ptr, PyLong_v(n__val)) &*&
     pyobj_hasval(k__ptr, PyLong_v(k__val));
 @*/
     {
         PyObject *r = PyLong_FromUnsignedLong(res);
-        //@ assert (r == NULL)?PyExc( some(_), some(_)):PyExc(e_1, t_1);
-        //@ assert (r == NULL)?true:pyobj_hasval(r, PyLong_v(res));
         PyErr_Clear();
-        //@ assert PyExc(none, none);
-        //@ assert (r == NULL)?true:pyobj_hasval(r, PyLong_v(res));
     }
-    //@ assert (r == NULL)?true:pyobj_hasval(r, PyLong_v(res));
     return r;
 }
