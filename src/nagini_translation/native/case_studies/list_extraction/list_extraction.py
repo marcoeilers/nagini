fad76669ed045251943fb4c1142c4594c5a1915d
etiennebirling <etienne.birling@gmail.com>
2025-04-24 20:56:40 +0200

    refactor: reorder Ensures clauses in first_swap function for improved clarity

--- src/nagini_translation/native/case_studies/list_extraction/list_extraction.py
+++ src/nagini_translation/native/case_studies/list_extraction/list_extraction.py
@@ -4,6 +4,8 @@ from typing import List
 @Native
 def first_swap(l:List[int], i:int) -> int:
     Requires(list_pred(l) and len(l) > 0)
-    Ensures(list_pred(l) and len(l) == Old(len(l)) and
-            Forall(int, lambda j: Implies(j >= 0 and j < len(l), l[j] is Old(l[j])))
-            and Result() is Old(l[i]))
+    Requires(len(l) > i and i >= 0 and len(l) < 100)
+    Ensures(list_pred(l) and len(l) == Old(len(l)))
+    Ensures(Result() is Old(l[i]))
+    Ensures(Forall(int, lambda j: Implies(j >= 0 and j < len(l), l[j] is Old(l[j]))))
+            
