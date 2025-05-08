9122b52873cd3a855a63b3fb2c17e0a89ff0dcfd
etiennebirling <etienne.birling@gmail.com>
2025-05-08 09:16:08 +0200

    fixed versioning problem in list_extraction.py... VFPY translation is already consistent

--- src/nagini_translation/native/case_studies/list_extraction/list_extraction.py
+++ src/nagini_translation/native/case_studies/list_extraction/list_extraction.py
@@ -2,10 +2,10 @@ from nagini_contracts.contracts import *
 from typing import List
 @ContractOnly
 @Native
-def first_swap(l:List[int], i:int) -> int:
-    Requires(list_pred(l) and len(l) > 0)
+def list_extraction(l:List[int], i:int) -> int:
+    Requires(list_pred(l))
     Requires(len(l) > i and i >= 0 and len(l) < 100)
     Ensures(list_pred(l) and len(l) == Old(len(l)))
-    Ensures(Result() is Old(l[i]))
     Ensures(Forall(int, lambda j: Implies(j >= 0 and j < len(l), l[j] is Old(l[j]))))
+    Ensures(Result() is Old(l[i]))
             
