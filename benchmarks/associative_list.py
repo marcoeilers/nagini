7bd65564faebb0fef3aa6ac96215b25e9fd73899
Micha <micha@example.com>
2025-09-15 11:21:31 +0200

    another try

--- benchmarks/associative_list.py
+++ benchmarks/associative_list.py
@@ -4,51 +4,6 @@
 from nagini_contracts.contracts import *
 from typing import cast, List, Set, Dict, Tuple, Optional
 
-# class Node:
-#     def __init__(self, k: int, v: object) -> None:
-#         Requires(Acc(state_pred(v)))
-#         Ensures(Acc(self.state()))
-#         Unfold(state_pred(v))
-#         self.data: Tuple[int, object] = (k, v)
-#         self.next: Node = None
-#         Fold(Acc(state_pred(self.data)))
-#         Fold(Acc(self.state()))
-# 
-#     @Predicate
-#     def state(self) -> bool:
-#         return Acc(self.data) and Acc(state_pred(self.data))
-# 
-# @Predicate
-# def lseg(start: Node, end: Node) -> bool:
-#     return Implies(not (start is end), Acc(start.state()) and Acc(start.next) and Acc(lseg(start.next, end)))
-# 
-# class Map:
-#     # list of key/value pairs
-#     def __init__(self) -> None:
-#         Ensures(self.state())
-#         self.head: Node = None
-#         Fold(lseg(self.head, None))
-#         Fold(self.state())
-# 
-#     @Predicate
-#     def state(self) -> bool:
-#         return Acc(self.head) and Acc(lseg(self.head, None))
-# 
-#     @Pure
-#     def lookup(self, key: int) -> Optional[object]:
-#         Requires(Acc(self.state()))
-#         # Ensures(Implies(key in self.m, Result() == self.m[key]))
-#         # Ensures(Implies(not (key in self.m), Result() == None))
-#         return None
-# 
-#         # Unfold(self.state())
-#         # res: object = None
-#         # for (k, v) in self.m:
-#         #     if k == key:
-#         #         res = v
-#         #         break
-#         # Fold(self.state())
-#         # return res
 
 class Map:
     # list of key/value pairs
@@ -94,6 +49,8 @@ class Map:
         Unfold(self.state())
         Unfold(state_pred(self.keys))
         Unfold(state_pred(self.values))
+        if self.keys == []:
+            return None
         res: Optional[object] = None
         i: int = 0
         while i < len(self.keys):
@@ -102,7 +59,7 @@ class Map:
             Invariant(Acc(self.keys) and Acc(self.values))
             Invariant(Acc(list_pred(self.keys)) and Acc(list_pred(self.values)))
 
-            Invariant(0 <= i and i <= len(self.keys))
+            Invariant(0 <= i and i < len(self.keys))
             Invariant(len(self.keys) == len(self.values))
             Invariant(res is None or Exists(
                 int, lambda j: 0 <= j and j < len(self.keys) and self.keys[j] == key and res is self.values[j]
@@ -119,3 +76,64 @@ class Map:
     @Predicate
     def state(self) -> bool:
         return Acc(self.keys) and Acc(self.values) and Acc(state_pred(self.keys)) and Acc(state_pred(self.values))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Node:
+#     def __init__(self, k: int, v: object) -> None:
+#         Requires(Acc(state_pred(v)))
+#         Ensures(Acc(self.state()))
+#         Unfold(state_pred(v))
+#         self.data: Tuple[int, object] = (k, v)
+#         self.next: Node = None
+#         Fold(Acc(state_pred(self.data)))
+#         Fold(Acc(self.state()))
+# 
+#     @Predicate
+#     def state(self) -> bool:
+#         return Acc(self.data) and Acc(state_pred(self.data))
+# 
+# @Predicate
+# def lseg(start: Node, end: Node) -> bool:
+#     return Implies(not (start is end), Acc(start.state()) and Acc(start.next) and Acc(lseg(start.next, end)))
+# 
+# class Map:
+#     # list of key/value pairs
+#     def __init__(self) -> None:
+#         Ensures(self.state())
+#         self.head: Node = None
+#         Fold(lseg(self.head, None))
+#         Fold(self.state())
+# 
+#     @Predicate
+#     def state(self) -> bool:
+#         return Acc(self.head) and Acc(lseg(self.head, None))
+# 
+#     @Pure
+#     def lookup(self, key: int) -> Optional[object]:
+#         Requires(Acc(self.state()))
+#         # Ensures(Implies(key in self.m, Result() == self.m[key]))
+#         # Ensures(Implies(not (key in self.m), Result() == None))
+#         return None
+# 
+#         # Unfold(self.state())
+#         # res: object = None
+#         # for (k, v) in self.m:
+#         #     if k == key:
+#         #         res = v
+#         #         break
+#         # Fold(self.state())
+#         # return res
