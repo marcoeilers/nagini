62f1cb85ad3c05f2b64c5edb1ab7667e4bf97db3
Micha <>
2025-09-28 14:01:00 +0200

    Fixed implementation of associative list

--- benchmarks/associative_list.py
+++ benchmarks/associative_list.py
@@ -2,89 +2,96 @@
 # http://creativecommons.org/publicdomain/zero/1.0/
 
 from nagini_contracts.contracts import *
-from typing import cast, List, Set, Dict, Tuple, Optional
+from typing import List, Optional
 
 
 class Map:
-    # list of key/value pairs
-    def __init__(self, keys: List[int], values: List[object]) -> None:
-        Requires(state_pred(keys))
-        Requires(state_pred(values))
+    def __init__(self, keys: List[object], values: List[object]) -> None:
+        Requires(Acc(self.keys))
+        Requires(Acc(self.values))
+        Ensures(Acc(self.keys))
+        Ensures(Acc(self.values))
+        Ensures(self.keys is keys and self.values is values)
         Ensures(self.state())
-        Ensures(Unfolding(self.state(), self.keys is keys and self.values is values))
         
-        self.keys: List[int] = keys
+        self.keys: List[object] = keys
         self.values: List[object] = values
+        Fold(state_pred(keys))
+        Fold(state_pred(values))
         Fold(self.state())
 
     @Pure
-    def __contains__(self, key: int) -> bool:
+    def __contains__(self, key: object) -> bool:
         Requires(Acc(self.state()))
+        Requires(Implies(not Stateless(key), Acc(state_pred(key))))
         Ensures(Unfolding(self.state(), Implies(key in self.keys, Result())))
         Ensures(Unfolding(self.state(), Implies(not (key in self.keys), not Result())))
-        return Unfolding(
-            self.state(),
+        return Unfolding(self.state(),
             key in self.keys
         )
     
-    def lookup(self, key: int) -> Optional[object]:
+    def lookup(self, key: object) -> Optional[object]:
         Requires(Acc(self.state()))
-        Requires(Unfolding(self.state(), Unfolding(state_pred(self.keys), Unfolding(state_pred(self.values), len(self.keys) == len(self.values)))))
+        Requires(Implies(not Stateless(key), Acc(state_pred(key))))
         Ensures(Acc(self.state()))
-        Ensures(Unfolding(self.state(), Unfolding(state_pred(self.keys), Unfolding(state_pred(self.values), len(self.keys) == len(self.values)))))
-        Ensures(
-            Unfolding(self.state(), Unfolding(state_pred(self.keys), Unfolding(state_pred(self.values),
-                Exists(int, lambda j:
-                    Implies(
-                        0 <= j and j < len(self.keys) and self.keys[j] == key,
-                        Result() is self.values[j]
-                    )
-                )
-            )))
-        )
-        Ensures(
-            Unfolding(self.state(), Unfolding(state_pred(self.keys),
+        Ensures(Implies(not Stateless(key), Acc(state_pred(key))))
+        
+        Ensures(Unfolding(self.state(), Unfolding(state_pred(self.keys), Unfolding(state_pred(self.values),
                 Implies(
-                    Forall(int, lambda j:
-                        Implies(0 <= j and j < len(self.keys), self.keys[j] != key)
-                    ),
-                    Result() is None
+                    not (Result() is None),
+                    Exists(int, lambda j: 0 <= j and j < len(self.keys) and j < len(self.values) and self.keys[j] == key and Result() is self.values[j])
                 )
-            ))
-        )
+        ))))
 
-        Unfold(self.state())
-        Unfold(state_pred(self.keys))
-        Unfold(state_pred(self.values))
         res: Optional[object] = None
         i: int = 0
-        while i < len(self.keys):
-            # permission to access both lists and their elements (read-only)
-            Invariant(Acc(self.keys, 1/2) and Acc(self.values, 1/2))
-            Invariant(Acc(list_pred(self.keys), 1/2) and Acc(list_pred(self.values), 1/2))
-            Invariant(Forall(self.values, lambda item: Implies(not Stateless(item), Acc(state_pred(item), 1/2))))
-            Invariant(Forall(self.keys, lambda item: Implies(not Stateless(item), Acc(state_pred(item), 1/2))))
+        found_idx: int = -1
 
-            Invariant(0 <= i and i <= len(self.keys))
-            Invariant(len(self.keys) == len(self.values))
-            Invariant(
-                Forall(int, lambda j:
-                    Implies(0 <= j and j < i, self.keys[j] != key)
-                )
-            )
-            Invariant(
-                Implies(res is not None,
-                    Exists(int, lambda j:
-                        0 <= j and j < i and self.keys[j] == key and res is self.values[j]
-                    )
-                )
-            )
-            if self.keys[i] == key:
-                res = self.values[i]
-                break
-            i += 1
+        Unfold(self.state())
+
+        Unfold(state_pred(self.keys))
+        Unfold(state_pred(self.values))
+        len_keys: int = len(self.keys)
+        len_values: int = len(self.values)
+        Assume(len_keys == len_values)
         Fold(state_pred(self.values))
         Fold(state_pred(self.keys))
+
+        while (i < len_keys and found_idx == -1):
+            Invariant(Acc(self.keys, 1/2) and Acc(self.values, 1/2))
+            Invariant(Implies(not Stateless(key), Acc(state_pred(key))))
+            Invariant(Acc(state_pred(self.keys)))
+            Invariant(Acc(state_pred(self.values)))
+            Invariant(
+              (res is None and found_idx == -1) or
+              (res is not None and found_idx != -1)
+            )
+            Invariant(
+                Implies(
+                    (res is not None) and (found_idx != -1),
+                    Unfolding(state_pred(self.keys), Unfolding(state_pred(self.values), 
+                        0 <= found_idx and found_idx < len(self.keys) and found_idx < len(self.values) and 
+                        self.keys[found_idx] == key and res is self.values[found_idx]
+                    ))
+                )
+            )
+
+            Unfold(state_pred(self.keys))
+            Unfold(state_pred(self.values))
+            Assume(0 <= i and i < len(self.keys))
+            Assume(0 <= i and i < len(self.values))
+
+            if key == self.keys[i]:
+                if res:
+                    res = self.values[i]
+                    found_idx = i
+                    i += 1
+                    break
+            i += 1
+
+            Fold(state_pred(self.values))
+            Fold(state_pred(self.keys))
+        
         Fold(self.state())
         return res
 
