8feaeb787c3b0bc3ba545927dfc7bc22b58d68ac
Micha <>
2025-10-12 14:45:58 +0200

    added some evaluation examples

--- evaluation_paper/fraction_int.py
+++ evaluation_paper/fraction_int.py
@@ -2,37 +2,35 @@
 # http://creativecommons.org/publicdomain/zero/1.0/
 
 from nagini_contracts.contracts import *
-from typing import cast, List, Tuple
+from typing import cast, List, Tuple, Optional
 
+
+# TODO: Int class: i: Optional[int] -> None ~= not defined
 
 class Fraction:
     def __init__(self, a: int, b: int):
         Ensures(self.state())
         Ensures(Unfolding(self.state(),
             Implies(
-                a != 0 and b > 0 and self._gcd(a, b) != 0, self.num is a // self._gcd(a, b) and self.denom is b // self._gcd(a, b) and not self.denom_zero
+                a != 0 and b > 0 and self._gcd(a, b) != 0, self.num is a // self._gcd(a, b) and self.denom is b // self._gcd(a, b)
             ) and
             Implies(
-                a != 0 and b < 0 and self._gcd(a, b) != 0, self.num is a // (-self._gcd(a, b)) and self.denom is b // (-self._gcd(a, b)) and not self.denom_zero
+                a != 0 and b < 0 and self._gcd(a, b) != 0, self.num is a // (-self._gcd(a, b)) and self.denom is b // (-self._gcd(a, b))
             ) and
             Implies(
-                a == 0 and b != 0, self.num == 0 and self.denom == 1 and not self.denom_zero
+                a == 0 and b != 0, self.num == a and self.denom == 1
             ) and
             Implies(
-                b == 0, self.num == a and self.denom == 1 and self.denom_zero
-            ) and
-            self.denom != 0
+                b == 0, self.num == 1 and self.denom == b
+            )
         ))
         _a: int = a
         _b: int = b
 
-        self.denom_zero: bool = False
-
-        if b == 0:
-            self.denom_zero = True
+        if a == 0 and b != 0:
             _b = 1
-        elif a == 0:
-            _b = 1
+        elif b == 0:
+            _a = 1
         else:
             # algorithm from fractions package
             g: int = self._gcd(a, b)
@@ -68,43 +66,72 @@ class Fraction:
             Implies(
                 isinstance(other, Fraction),
                 Unfolding(self.state(), Unfolding(state_pred(other),
-                    Result() == (self.num == cast(Fraction, other).num and
-                                 self.denom == cast(Fraction, other).denom)
+                    Result() == (
+                        self.num == cast(Fraction, other).num and
+                        self.denom == cast(Fraction, other).denom
+                    )
                 ))
             )
-        )
+        ) 
+        # Ensures(
+        #     Implies(
+        #         isinstance(other, Int),
+        #         Unfolding(self.state(), Unfolding(state_pred(other),
+        #             Result() == (cast(Int, other).denom != 0 and
+        #                         self.num == cast(Fraction, other).num and
+        #                         self.denom == cast(Fraction, other).denom)
+        #         ))
+        #     )
+        # ) 
         Ensures(
             Implies(
                 isinstance(other, Int),
                 Unfolding(self.state(), Unfolding(state_pred(other),
-                    Implies(
-                        self.denom != 0 and not self.denom_zero and self.num % self.denom == 0,
-                        Result() == ((self.num // self.denom) == cast(Int, other).i)
+                    Result() == (
+                        (self.num == 1 and self.denom == 0 and cast(Int, other).i is None) or
+                        (self.denom != 0 and not cast(Int, other).i is None) and self.num == cast(Int, other).i and self.denom == 1)
                     )
-                ))
+                )
             )
         )
+        # Ensures(Implies(Unfolding(self.state(), self.denom == 0), not Result()))
+        # Ensures(Implies(isinstance(other, Fraction) and Unfolding(state_pred(other), cast(Fraction, other).denom == 0), not Result()))
+        # Ensures(
+        #     Implies(
+        #         isinstance(other, Int),
+        #         Unfolding(self.state(), Unfolding(state_pred(other),
+        #             Result() == (
+        #                 self.denom != 0 and not self.denom_zero and (self.num % self.denom == 0) and 
+        #                 (self.num // self.denom) == cast(Int, other).i
+        #             )
+        #         ))
+        #     )
+        # )
+        if self is other:
+            return True
         if isinstance(other, Fraction):
             return Unfolding(self.state(), Unfolding(state_pred(other),
-                    (self.num == cast(Fraction, other).num and
-                     self.denom == cast(Fraction, other).denom)
+                self.num == cast(Fraction, other).num and
+                self.denom == cast(Fraction, other).denom
             ))
         elif isinstance(other, Int):
-            if Unfolding(self.state(), self.denom != 0 and not self.denom_zero and self.num % self.denom == 0):
-                return Unfolding(self.state(), Unfolding(state_pred(other), (self.num // self.denom) == cast(Int, other).i))
+            return Unfolding(self.state(), Unfolding(state_pred(other),
+                (self.num == 1 and self.denom == 0 and cast(Int, other).i is None) or
+                (self.denom != 0 and not cast(Int, other).i is None) and self.num == cast(Int, other).i and self.denom == 1)
+            )
         return False
 
     @Predicate
     def state(self) -> bool:
-        return Acc(self.num) and Acc(self.denom) and Acc(self.denom_zero) and self.denom != 0
+        return Acc(self.num) and Acc(self.denom)
 
 
 class Int:
-    def __init__(self, i: int):
+    def __init__(self, i: Optional[int]):
         Ensures(self.state())
         Ensures(Unfolding(self.state(), self.i is i))
 
-        self.i: int = i
+        self.i: Optional[int] = i
         Fold(self.state())
 
     @Pure
@@ -116,7 +143,8 @@ class Int:
             Implies(
                 isinstance(other, Int),
                 Unfolding(self.state(), Unfolding(state_pred(other),
-                    Result() == (self.i == cast(Int, other).i)
+                    Result() == (self.i is None and cast(Int, other).i is None or 
+                                 not (self.i is None or cast(Int, other).i is None) and self.i == cast(Int, other).i)
                 ))
             )
         )
@@ -124,21 +152,54 @@ class Int:
             Implies(
                 isinstance(other, Fraction),
                 Unfolding(self.state(), Unfolding(state_pred(other),
-                    Implies(
-                        (cast(Fraction, other).denom != 0 and not cast(Fraction, other).denom_zero) and
-                        cast(Fraction, other).num % cast(Fraction, other).denom == 0,
-                        Result() == (self.i == (cast(Fraction, other).num // cast(Fraction, other).denom))
+                    Result() == (
+                        (self.i is None and cast(Fraction, other).num == 1 and cast(Fraction, other).denom == 0) or
+                        (not (self.i is None) and cast(Fraction, other).denom != 0 and
+                         cast(Fraction, other).num == self.i and cast(Fraction, other).denom == 1)
                     )
                 ))
             )
-        )
+        ) 
+        # Ensures(
+        #     Implies(
+        #         isinstance(other, Int),
+        #         Unfolding(self.state(), Unfolding(state_pred(other),
+        #             Result() == (self.i == cast(Int, other).i)
+        #         ))
+        #     )
+        # )
+        # Ensures(
+        #     Implies(
+        #         isinstance(other, Fraction),
+        #         Unfolding(self.state(), Unfolding(state_pred(other),
+        #             Result() == (
+        #                 (cast(Fraction, other).denom != 0 and not cast(Fraction, other).denom_zero) and
+        #                 (cast(Fraction, other).num % cast(Fraction, other).denom == 0) and
+        #                 (self.i == (cast(Fraction, other).num // cast(Fraction, other).denom))
+        #             )
+        #         ))
+        #     )
+        # )
+        # if isinstance(other, Int):
+        #     return Unfolding(self.state(), self.i) == Unfolding(state_pred(other), cast(Int, other).i)
+        # if isinstance(other, Fraction):
+        #     return Unfolding(self.state(), Unfolding(state_pred(other),
+        #         cast(Fraction, other).denom != 0 and not cast(Fraction, other).denom_zero and (cast(Fraction, other).num % cast(Fraction, other).denom == 0) and
+        #         (self.i == cast(Fraction, other).num // cast(Fraction, other).denom)
+        #     ))
+        # return False
+        if self is other:
+            return True
         if isinstance(other, Int):
-            return Unfolding(self.state(), self.i) == Unfolding(state_pred(other), cast(Int, other).i)
-        if isinstance(other, Fraction):
-            if Unfolding(state_pred(other), cast(Fraction, other).denom_zero):
-                return False
-            if Unfolding(state_pred(other), cast(Fraction, other).denom != 0 and not cast(Fraction, other).denom_zero and cast(Fraction, other).num % cast(Fraction, other).denom == 0):
-                return Unfolding(self.state(), Unfolding(state_pred(other), (self.i == cast(Fraction, other).num // cast(Fraction, other).denom)))
+            return Unfolding(self.state(), Unfolding(state_pred(other),
+                (self.i is None and cast(Int, other).i is None) or 
+                (not (self.i is None or cast(Int, other).i is None) and self.i == cast(Int, other).i)
+            ))
+        elif isinstance(other, Fraction):
+            return Unfolding(self.state(), Unfolding(state_pred(other),
+                (self.i is None and cast(Fraction, other).num == 1 and cast(Fraction, other).denom == 0) or
+                (not (self.i is None) and cast(Fraction, other).denom != 0 and cast(Fraction, other).num == self.i and cast(Fraction, other).denom == 1)
+            ))
         return False
 
     @Predicate
@@ -146,10 +207,18 @@ class Int:
         return Acc(self.i)
 
 
-if __name__ == '__main__':
-    f: Fraction = Fraction(8, 4)
-    if Unfolding(f.state(), not f.denom_zero):
-        i: Int = Int(2)
-        # Fold(state_pred(f))
-        # Fold(state_pred(i))
-        assert f == i
+# if __name__ == '__main__':
+#     f: Fraction = Fraction(8, 4)
+#     i: Int = Int(2)
+#     Assert(f == i)
+#     Assert(i == f)
+
+    # ff: Fraction = Fraction(17, 0)
+    # fff: Fraction = Fraction(2, 0)
+    # ii: Int = Int(None)
+    # Assert(ff == ii)
+    # Assert(ii == ff)
+    # Assert(ff == fff)
+    # Assert(fff == ff)
+    # Assert(fff == ii)
+    # Assert(ii == fff)
---
+++ evaluation_paper/stateless.py
@@ -0,0 +1,23 @@
+# Any copyright is dedicated to the Public Domain.
+# http://creativecommons.org/publicdomain/zero/1.0/
+
+from nagini_contracts.contracts import *
+from typing import Tuple
+
+
+def client_tuple(x: Tuple[Tuple[int, str], bool], y: Tuple[Tuple[int, str], bool]) -> str:
+    Requires(Stateless(x))
+    Requires(Stateless(y))
+    Requires(x[0][0] == y[0][0])
+    Requires(x[0][1] == y[0][1])
+    Requires(x[0] == y[0])
+    Requires(x[1] == y[1])
+    Ensures(Result() == x[0][1])
+    assert x == y
+    return x[0][1]
+
+
+if __name__ == '__main__':
+    x: Tuple[Tuple[int, str], bool] = ((42, "hello"), True)
+    y: Tuple[Tuple[int, str], bool] = ((42, "hello"), True)
+    Assert(client_tuple(x, y) == "hello")
