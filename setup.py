39ae29749c3e4c836bbf387ec6acb3e639c04f03
marcoeilers <eilers.marco@googlemail.com>
2025-11-26 21:26:07 +0100

    Adding pytest-instafail to debug test failures in CI

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
             'astunparse==1.6.2',
             'pytest==4.3.0',
             'z3-solver==4.8.7.0',
+            'pytest-instafail'
             ],
         entry_points={
              'console_scripts': [
