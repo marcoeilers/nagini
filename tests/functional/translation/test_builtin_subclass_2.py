19e812495c2d06c61203a68b12c7f796be6ccd0b
Marco Eilers <marcoeilers@users.noreply.github.com>
2025-12-17 18:29:38 +0100

    Subclassing int (#262)
    * Subclassing int
    
    * Translation tests for unsupported errors, fixed crash in previous version

---
+++ tests/functional/translation/test_builtin_subclass_2.py
@@ -0,0 +1,7 @@
+# Any copyright is dedicated to the Public Domain.
+# http://creativecommons.org/publicdomain/zero/1.0/
+
+
+#:: ExpectedOutput(unsupported:Subclassing builtin type is currently not supported.)
+class A(list[int]):
+    pass
