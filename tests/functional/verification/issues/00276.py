fa9fde298a2adf0af52fd3028aceba8a6f62a5e6
marcoeilers <eilers.marco@googlemail.com>
2026-01-19 17:11:49 +0100

    Same fix for function calls

--- tests/functional/verification/issues/00276.py
+++ tests/functional/verification/issues/00276.py
@@ -21,3 +21,21 @@ def test_produce_seq() -> A[PSeq[int]]:
     Ensures(isinstance(Result().value, PSeq))
     Ensures(len(Result().value) == 3)
     return A[PSeq[int]](PSeq(1,2,3))
+
+
+class A2(Generic[T]):
+
+    def __init__(self, val: Optional[T] = None) -> None:
+        Ensures(Acc(self._value) and self._value is val) # type: ignore
+        self._value = val
+
+    @Pure
+    def value(self) -> Optional[T]:
+        Requires(Acc(self._value))
+        return self._value
+
+def test_produce_seq2() -> A2[PSeq[int]]:
+    Ensures(Acc(Result()._value))
+    Ensures(isinstance(Result().value(), PSeq))
+    Ensures(len(Result().value()) == 3)
+    return A2[PSeq[int]](PSeq(1,2,3))
