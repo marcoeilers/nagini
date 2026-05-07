1e69fb5c6dfda474bdfeeadf33eef75259858219
marcoeilers <eilers.marco@googlemail.com>
2026-05-07 15:10:39 +0200

    Less ugly code

--- src/nagini_translation/lib/errors/manager.py
+++ src/nagini_translation/lib/errors/manager.py
@@ -148,33 +148,28 @@ class ErrorManager:
             while iterator.hasNext():
                 bc = iterator.next()
                 bc_pos = bc.pos()
+                negated = False
                 if isinstance(bc, jvm.viper.silver.ast.Not) and bc_pos == bc.exp().pos():
-                    inner = bc.exp()
-                    inner_item = self._get_item(inner.pos())
-                    if not inner_item:
-                        continue
-                    condition = (inner_item.reason_string or pprint(inner_item.node))
-                    pos = str(inner.pos())
-                    if '.sil' in pos:
-                        continue
-                    if pos == '<no position>':
-                        pos = ''
+                    negated = True
+                    bc = bc.exp()
+                    bc_pos = bc.pos()
+                bc_item = self._get_item(bc_pos)
+                if not bc_item:
+                    continue
+                condition = (bc_item.reason_string or pprint(bc_item.node))
+                if negated:
+                    if condition.startswith('(not ') and condition.endswith(')'):
+                        condition = condition[5:-1]
                     else:
-                        pos = ' at ' + pos
-                    py_bcs.append("(not {0}){1}".format(condition, pos))
+                        condition = '(not {0})'.format(condition)
+                pos = str(bc_pos)
+                if '.sil' in pos:
+                    continue
+                if pos == '<no position>':
+                    pos = ''
                 else:
-                    bc_item = self._get_item(bc_pos)
-                    if not bc_item:
-                        continue
-                    condition = (bc_item.reason_string or pprint(bc_item.node))
-                    pos = str(bc_pos)
-                    if '.sil' in pos:
-                        continue
-                    if pos == '<no position>':
-                        pos = ''
-                    else:
-                        pos = ' at ' + pos
-                    py_bcs.append("{0}{1}".format(condition, pos))
+                    pos = ' at ' + pos
+                py_bcs.append("{0}{1}".format(condition, pos))
         if error_item:
             return Error(error, rules, reason_item, error_item.node,
                          error_item.vias, inputs=inputs, bcs=py_bcs)
